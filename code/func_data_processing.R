2946c5afc0cf1894c25ac7a695154c4b2245c30b
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-16 15:10:34 +1000

    Add logical to specify whether to output model chunk columns

--- code/func_data_processing.R
+++ code/func_data_processing.R
@@ -3,7 +3,7 @@
 
 # Functions for manipulating, processing, and preparing datasets
 
-extract.partition.models <- function(partition_file, paper_id){
+extract.partition.models <- function(partition_file, paper_id, output.model.chunks = FALSE){
   # Function to read in a partition file and return the list of models applied to the charsets
   
   # Open the partition file
@@ -14,39 +14,48 @@ extract.partition.models <- function(partition_file, paper_id){
   end_ind <- grep("end", p, ignore.case = T)[which(grep("end", p, ignore.case = T) > start_ind)]
   # Extract all lines in the charpartition section
   charpartition_lines <- p[start_ind:end_ind]
+  
   # Identify all lines containing a colon (":") - these will specify a model for a charset
   model_lines <- grep(":", charpartition_lines, value = T)
   # Split the lines at the colon and select the first section of each line
   models <- unlist(lapply(model_lines, 
                           function(l){gsub(" ", "", strsplit(l, ":")[[1]][[1]])} ))
+  
   # Identify how many times each model was used
   model_t <- table(models)
   model_count <- as.numeric(model_t)
   model_name <- names(model_t)
+  
   # Construct a dataframe with the model name and information
   model_df <- data.frame(paper_id = paper_id,
                          partition_file = basename(partition_file),
                          model = model_name,
-                        count = model_count)
+                         count = model_count)
   # Get the starting names from the model_df
   model_df_init_names <- names(model_df)
-  # Find the maximum number of chunks that belong to each model
-  max_number_chunks <- max(unlist(lapply(strsplit(model_name, "\\+"), length)))
-  # Break each model down into the component parts
-  for (i in 1:max_number_chunks){
-    # Extract the chunks at this position
-    model_chunks <- unlist(lapply(model_name, get.model.chunk, i))
-    # Add the chunks at this position to the model dataframe
-    model_df <- cbind(model_df, model_chunks)
-    # Create a column name for this position
-    col_names <- c(model_df_init_names, paste0("model_position_",1:i))
-    # Rename the columns for the model dataframe
-    names(model_df) <- col_names
-  } # end for (i in 1:max_number_chunks)
+  
+  # If desired, output columns for the model chunks
+  if (output.model.chunks == TRUE){
+    # Find the maximum number of chunks that belong to each model
+    max_number_chunks <- max(unlist(lapply(strsplit(model_name, "\\+"), length)))
+    # Break each model down into the component parts
+    for (i in 1:max_number_chunks){
+      # Extract the chunks at this position
+      model_chunks <- unlist(lapply(model_name, get.model.chunk, i))
+      # Add the chunks at this position to the model dataframe
+      model_df <- cbind(model_df, model_chunks)
+      # Create a column name for this position
+      col_names <- c(model_df_init_names, paste0("model_position_",1:i))
+      # Rename the columns for the model dataframe
+      names(model_df) <- col_names
+    } # end for (i in 1:max_number_chunks)
+  } # end if (output.model.chunks == TRUE)
+  
   # Check whether each model contains +I, +F, or +G. Add results to model dataframe
   model_df$plus_f <- grepl("\\+F", model_name)
   model_df$plus_i <- grepl("\\+I\\+", model_name)
   model_df$plus_g <- grepl("\\+G", model_name)
+  
   # Return model dataframe
   return(model_df)
 }
