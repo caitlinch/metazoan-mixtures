c32e400c4bdf99196e43b4242af663886fa6a399
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-16 15:07:39 +1000

    Extract list of models from all Redmond et al 2021 partition files

--- code/Redmond_extracting_all_models.R
+++ code/Redmond_extracting_all_models.R
@@ -6,8 +6,16 @@
 # supp_dir <- directory containing supplementary data from Redmond paper(downloaded from repository)
 
 supp_dir <- "/Users/caitlin/Downloads/RedmondMcLysaght2021NatCommsData" 
+mandir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
 
-## 1. Extract vector of all models used in Redmond 2021
+
+
+#### 1. Prepare variables, packages, and functions
+source(paste0(main_dir, "code/func_data_processing.R"))
+
+
+
+#### 2. Extract vector of all models used in Redmond 2021 ####
 # List all files in the supplementary data
 all_files <- list.files(supp_dir, recursive = T, full.names = T)
 # Extract files for the three sponge datasets (REA, WEA15 and WEA17)
@@ -16,3 +24,7 @@ sponge_files <- grep("REA|WEA15|WEA17", all_files, value = T)
 sponge_partition_files <- grep("partition", sponge_files, value = T)
 # Remove recoded analyses
 sponge_partition_files <- grep("RL1|RL2", sponge_partition_files, value = T, invert = T)
+# Extract dataframe of models from all files
+sponge_partition_model_list <- lapply(sponge_partition_files, extract.partition.models, "Redmond2021")
+
+
