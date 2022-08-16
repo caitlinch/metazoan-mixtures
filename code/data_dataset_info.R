18ce7e8810abd2cfd37f9c56caf92410a911aca6
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-16 15:19:24 +1000

    Added vector of unique models from charpartitions in Redmond et al 2021 paper

--- code/data_dataset_info.R
+++ code/data_dataset_info.R
@@ -3,6 +3,7 @@
 
 # File to store the taxa names, clade classifications, and models of sequence evolution for the Metazoan datasets used for analysis
 
+#### PHYLOGENETIC DATASETS ####
 # For Whelan et. al. (2017):
 whelan2017_list <- list("Bilateria" = c("Homo_sapiens", "Strongylocentrotus_purpatus", "Hemithris_psittacea", "Capitella_teleta", "Drosophila_melanogaster","Daphnia_pulex"),
                         "Cnidaria" = c("Hydra_vulgaris", "Bolocera_tuediae", "Aiptasia_pallida", "Hormathia_digitata", "Nematostella_vectensis", "Acropora_digitifera", 
@@ -33,3 +34,17 @@ whelan2017_list <- list("Bilateria" = c("Homo_sapiens", "Strongylocentrotus_purp
 
 # Create one list that contains all other lists, indexed by dataset identifier (first author+year of publication)
 all_datasets <- list("Whelan2017" = whelan2017_list)
+
+
+
+#### MODELS ####
+redmond_models <- c("C30", "C40", "C50", "C60", 
+                    "EHO+G", "EX_EHO+G", "EX2+G", "EX3+G", 
+                    "JTT+C20", "JTT+C30", "JTT+C40", "JTT+C50", "JTT+C60", "JTT+G4", 
+                    "JTTDCMut+G4", "JTTDCMut+I+G4", 
+                    "LG+C30", "LG+C40", "LG+C50", "LG+C60", "LG+CF4", "LG+F+G4", "LG+F+I+G4", "LG+G4", "LG+I+G4", "LG4M", 
+                    "mtZOA+F+G4", "mtZOA+G4", "mtZOA+I+G4", 
+                    "PMB+G4", 
+                    "rtREV+G4", "rtREV+I+G4", 
+                    "UL2+G", "UL3+G", 
+                    "WAG+C20", "WAG+C30", "WAG+C40", "WAG+C50", "WAG+C60", "WAG+G4", "WAG+I+G4")
