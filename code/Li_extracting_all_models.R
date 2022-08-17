7328e01d86ac1ee12f88d41a4f91f44e5d5fe054
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-17 15:42:15 +1000

    New file to extract all models of sequence evolution used in Li et al 2021 analyses

---
+++ code/Li_extracting_all_models.R
@@ -0,0 +1,14 @@
+# Input parameters
+# supp_dir <- directory containing supplementary data from Li et. al. (2021) paper (downloaded from repository)
+
+supp_dir <- "/Users/caitlin/Downloads/" 
+mandir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/animal_tree_root-main/"
+
+
+
+#### 1. Prepare variables, packages, and functions
+source(paste0(main_dir, "code/func_data_processing.R"))
+
+
+
+#### 2. Extract vector of all models used in Li et. al. (2021) ####
