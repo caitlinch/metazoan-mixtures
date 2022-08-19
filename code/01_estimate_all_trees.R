7f6a9699f518b634519ed1297036dbd2e3f0a512
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-19 13:49:12 +1000

    Add parameters for run

--- code/01_estimate_all_trees.R
+++ code/01_estimate_all_trees.R
@@ -10,10 +10,31 @@
 
 
 #### 1. Parameters ####
-## Specify directories:
+## Specify parameters:
+# alignment_dir     <- Directory containing alignments for all datasets
+# output_dir        <- Directory for IQ-Tree output (trees and tree mixtures)
+# repo_dir          <- Location of caitlinch/metazoan-mixtures github repository
+
+# iqtree2           <- Location of IQ-Tree2 stable release
+# iqtree_tm         <- Location of IQ-Tree2 MAST release
+
+location = "soma"
+if (location == "local"){
+  alignment_dir <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/01_alignments/"
+  output_dir <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/02_output/"
+  repo_dir <- "/Users/caitlin/Repositories/metazoan-mixtures/"
+  
+  iqtree2 <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/iqtree-2.2.0-MacOSX/bin/iqtree2"
+  iqtree2_tm <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/iqtree-2.2.0.7.mix-MacOSX/bin/iqtree2"
+  
+} if (location == "soma"){
+  alignment_dir <- ""
+  output_dir <- ""
+  repo_dir <- ""
+}
 
 
-
+#### 2. Process each dataset for each model of sequence evolution ####
 # - Take a single alignment:
 #   - Take a single model of sequence evolution
 #     - Use ModelFinder to determine the best model within that model category
