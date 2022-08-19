4194c8d4b266591b535c4d1acc881426644066cc
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-19 14:13:49 +1000

    Extract and separate list of models. Extract details about alignment from file name.

--- code/01_estimate_all_trees.R
+++ code/01_estimate_all_trees.R
@@ -1,17 +1,19 @@
 # metazoan-mixtures/code/01_estimate_all_trees.R
-## This script estimates maximum likelihood trees and maximum likelihood under constraint trees for empirical datasets
+## This script estimates maximum likelihood trees and maximum likelihood under constraint trees for empirical data sets
 # Caitlin Cherryh, 2022
 
 ## This script:
-# 1. Estimates ML trees for empirical datasets under different models
-# 2. Estimates ML trees using a constraint tree for empirical datasets under different models
+# 1. Estimates ML trees for empirical data sets under different models
+# 2. Estimates ML trees using a constraint tree for empirical data sets under different models
 # 3. Applies the MAST (Mixtures Across Sites and Trees) model 
 
 
 
 #### 1. Input parameters ####
 ## Specify parameters:
-# alignment_dir     <- Directory containing alignments for all datasets
+# alignment_dir     <- Directory containing alignments for all data sets
+#                      Alignments have the naming convention dataset.matrix_name.sequence_type.fa
+#                      E.g. Cherryh2022.all_taxa.aa.fa
 # output_dir        <- Directory for IQ-Tree output (trees and tree mixtures)
 # repo_dir          <- Location of caitlinch/metazoan-mixtures github repository
 
@@ -20,7 +22,7 @@
 
 location = "local"
 if (location == "local"){
-  alignment_dir <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/01_alignments/"
+  alignment_dir <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/01_alignments"
   output_dir <- "/Users/caitlin/Documents/PhD/Ch03_sponge_mixtures/02_output/"
   repo_dir <- "/Users/caitlin/Repositories/metazoan-mixtures/"
   
@@ -38,7 +40,6 @@ if (location == "local"){
 
 
 
-
 #### 2. Prepare functions, variables and packages ####
 # Source functions
 source(paste0(repo_dir, "code/func_constraint_trees.R"))
@@ -49,10 +50,31 @@ source(paste0(repo_dir, "code/data_dataset_info.R"))
 
 # Sort and categorise models of sequence evolution 
 all_models <- sort(unique(unlist(lapply(all_models, sort.model.chunks))))
+# Identify unique matrices within the models
+model_components <- sort(unique(unlist(strsplit(all_models, "\\+"))))
+# Remove components that do not correspond to a matrix or model (e.g. remove "I", "F", "G4")
+model_components <- model_components[!model_components %in% c("F", "FO", "G", "G4", "I", "R", "R4")]
+# PMSF is more complex than other models - requires separate function
+# For now, remove PMSF from list 
+model_components <- model_components[!model_components == "PMSF"]
+# Note: partitioning schemes currently not possible in mixture of trees implementation
 
 
 
 #### 3. Process each dataset for each model of sequence evolution ####
+# Extract the list of alignments
+all_alignments <- list.files(alignment_dir, full.names = T)
+
+# For each alignment:
+for (a in all_alignments){
+  # Extract details about alignment from file name
+  # Identify dataset (original paper citation)
+  a_dataset <- strsplit(basename(a), "\\.")[[1]][1]
+  # Identify matrix (which alignment file from original reference)
+  a_matrix_id <- strsplit(basename(a), "\\.")[[1]][2]
+}
+
+
 # - Take a single alignment:
 #   - Take a single model of sequence evolution
 #     - Use ModelFinder to determine the best model within that model category
