97e0965d175800099f1388c7bf23c47d4c84b76f
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-06 14:32:57 +1100

    Correct function to collate hypothesis trees - only collect files ending in .treefile

--- code/func_constraint_trees.R
+++ code/func_constraint_trees.R
@@ -941,13 +941,14 @@ combine.hypothesis.trees <- function(tree_id, constraint_tree_directory, outgrou
   tree_id_files <- grep(tree_id, all_constraint_tree_dir_files, value = TRUE)
   # Find all hypothesis trees for this tree_id (hypothesis trees are marked by HX, where 1<= X <= 5)
   hypothesis_tree_files <- grep("H1|H2|H3|H4|H5", tree_id_files, value = TRUE)
+  hypothesis_tree_treefiles <- grep("treefile", tree_id_files, value = TRUE)
   # Extend file path
-  if (length(hypothesis_tree_files) > 0){
-    hypothesis_tree_files <- paste0(constraint_tree_directory, hypothesis_tree_files)
+  if (length(hypothesis_tree_treefiles) > 0){
+    hypothesis_tree_treefiles <- paste0(constraint_tree_directory, hypothesis_tree_treefiles)
   }
   
   # Read in hypothesis tree files
-  hypothesis_trees <- lapply(hypothesis_tree_files, read.tree)
+  hypothesis_trees <- lapply(hypothesis_tree_treefiles, read.tree)
   # Convert hypothesis_trees from a list into a multiPhylo object 
   class(hypothesis_trees) <- "multiPhylo"
   
@@ -955,7 +956,7 @@ combine.hypothesis.trees <- function(tree_id, constraint_tree_directory, outgrou
   unrooted_file <- paste0(constraint_tree_directory, tree_id, "_unrooted_hypothesis_trees.tre")
   write.tree(hypothesis_trees, file = unrooted_file)
   
-  if (is.na(outgroup_taxa) == FALSE){
+  if (class(outgroup_taxa) == "character"){
     # If the outgroup taxa are provided, root the hypothesis trees and save the rooted trees
     # Root hypothesis trees
     rooted_hypothesis_trees <- root(hypothesis_trees, outgroup_taxa)
@@ -965,7 +966,7 @@ combine.hypothesis.trees <- function(tree_id, constraint_tree_directory, outgrou
     # Return paths for both rooted and unrooted hypothesis trees
     op_vec <- c(rooted_file, unrooted_file)
     names(op_vec) <- c("rooted_hypothesis_tree_file", "unrooted_hypothesis_tree_file")
-  } else if (is.na(outgroup_taxa) == TRUE){
+  } else if (class(outgroup_taxa) == "logical"){
     # If the outgroup taxa are not provided, return only the path to the unrooted hypothesis trees
     op_vec <- c(unrooted_file)
     names(op_vec) <- c("unrooted_hypothesis_tree_file")
