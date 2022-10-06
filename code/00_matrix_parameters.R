fd341edfe84df1e8a638f2eba88b0f96cb7a94f0
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-06 13:17:58 +1100

    Reformat nexus files to fasta (so can read in R). Extract matrix dimensions

--- code/00_matrix_parameters.R
+++ code/00_matrix_parameters.R
@@ -25,15 +25,16 @@ if (location == "local"){
 
 #### 2. Open libraries ####
 # Open packages
-library(ape)
 library(phylotools)
+library(TreeTools)
+library(phangorn)
 
 # Source functions
 source(paste0(repo_dir, "code/func_data_analysis.R"))
 
 
 
-#### 3. Extract matrix dimensions ####
+#### 3. Prepare alignment paths ####
 # Extract the list of all files from the folder containing alignments/models/partition files
 all_files <- list.files(alignment_dir)
 if (length(all_files) > 0){
@@ -42,6 +43,41 @@ if (length(all_files) > 0){
 # Extract the list of alignments (i.e. files that contain the word "alignment")
 all_alignments <- grep("\\.alignment\\.", all_files, value = T)
 
+# Two alignment paths need to be corrected - Dunn2008 and Hejnol2009
+# To correct Dunn 2008 (issue in reading file - read in as phyDat and write out as fasta file):
+corrected_dunn_file <- grep("FixedNames", grep("Dunn2008", all_alignments, value = T), value = T)
+if (file.exists(corrected_dunn_file) == FALSE){
+  # Get name of original alignment file
+  dunn_al <- grep("Original", grep("Dunn2008", all_alignments, value = T), value = T)
+  # Open alignment as phyDat
+  dunn_data <- ReadAsPhyDat(dunn_al)
+  # Write alignment as fasta file
+  write.phyDat(dunn_data, file = corrected_dunn_file, format = "fasta", colsep = "") 
+}
+# To correct Dunn 2008 (issue in reading file - read in as phyDat and write out as fasta file):
+corrected_hejnol_file <- grep("Original", grep("Hejnol2009", all_alignments, value = T), value = T)
+if (file.exists(corrected_hejnol_file) == FALSE){
+  # Get name of original alignment file
+  hejnol_al <- grep("FixedNames", grep("Hejnol2009", all_alignments, value = T), value = T)
+  # Open alignment as phyDat
+  hejnol_data <- ReadAsPhyDat(hejnol_al)
+  # Write alignment as fasta file
+  write.phyDat(hejnol_data, file = corrected_hejnol_file, format = "fasta", colsep = "")
+}
+
+# Re-extract the list of alignments and remove the files containing the word "Original"
+# Extract the list of all files from the folder containing alignments/models/partition files
+all_files <- list.files(alignment_dir)
+if (length(all_files) > 0){
+  all_files <- paste0(alignment_dir, all_files)
+}
+# Extract the list of alignments (i.e. files that contain the word "alignment")
+all_alignments <- grep("\\.alignment\\.", all_files, value = T)
+all_alignments <- grep("Original", all_alignments, value = T, invert = T)
+
+
+
+#### 4. Extract matrix dimensions ####
 # Extract information about each alignment
 dimension_list <- lapply(all_alignments, matrix.dimensions)
 dimension_df <- as.data.frame(do.call(rbind, dimension_list))
@@ -51,7 +87,3 @@ names(dimension_df) <- c("dataset", "matrix_name", "sequence_format", "num_taxa"
 df_path <- paste0(output_dir, "alignment_dimensions.csv")
 write.csv(dimension_df, file = df_path, row.names = FALSE)
 
-
-# Broken:
-# "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/01_Data_all/Dunn2008.Dunn2008.aa.alignment.nex"  [3]
-# "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/01_Data_all/Hejnol2009.Hejnol_etal_2009.aa.alignment.nex [4]
