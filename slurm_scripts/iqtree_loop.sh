9dcf59a35eb0726548120f3f60eae6179ac71ff1
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-07 15:22:56 +1100

    Remove .bak files

--- slurm_scripts/iqtree_loop.sh
+++ slurm_scripts/iqtree_loop.sh
@@ -8,16 +8,20 @@ cd /Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/ML_iqtree_s
 for i in {1..432} 
 do
 	# Copy the template file to a new file where the name includes the row number
-	cp /Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/iqtree_format.sh iqtree_run_$i.sh
+	cp /Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/slurm_scripts/iqtree_format.sh iqtree_run_$i.sh
 	# Update the row number in the new file for the sbatch commands
 	sed -i'.bak' "s/\${id}/$i/" iqtree_run_$i.sh
 	# Output the iqtree command for this row
 	head -n +$(($i+1)) /Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/maximum_likelihood_iqtree2_calls.csv | tail -n1 >> iqtree_run_$i.sh
 done
 
+# Remove all files with .bak extension
+ls *.bak
+rm *.bak
+
 # Create a list of all the .sh files
-ls > /Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/iqtree_runs.sh
+ls > /Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/iqtree_individual_job_runs.sh
 
 # Add the prefix "sbatch " to each line (to call all  432 jobs at once)
-sed -i'.bak' 's/^/sbatch /' /Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/iqtree_runs.sh
+sed -i'.bak' 's/^/sbatch /' /Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/iqtree_individual_job_runs.sh
 
