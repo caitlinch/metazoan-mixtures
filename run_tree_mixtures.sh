d255bf6a1b0477137ad2a50fdbc96784e3d6187a
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-07 12:15:00 +1100

    Added .sh file to run tree mixtures remotely on server

---
+++ run_tree_mixtures.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#SBATCH --job-name=tree_mixtures
+#SBATCH --output={SLURM_JOBID}.out
+#SBATCH --error=test.err 
+#SBATCH --partition=Standard
+#
+#SBATCH --time=3:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=100
+#
+##SBATCH --mail-user u5348329@anu.edu.au
+##SBATCH --mail-type ALL
+
+# Change to working directory for this project
+cd /home/u5348329/metazoan-mixtures/
+
+# Activate Anaconda work environment
+source /home/u5348329/.bashrc
+source conda activate tree_mixtures
+
+# Run Rscript in a clean R instance, output a logfile
+Rscript --vanilla --verbose /code/01_estimate_all_trees_parallel.R 2
+
+# append logfile to this scripts logfile
+cat slurm-${SLURM_JOBID}.Rout >> slurm-${SLURM_JOBID}.out
+
+# remove Rout log
+rm slurm-${SLURM_JOBID}.Rout
