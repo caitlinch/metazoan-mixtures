a172338ea6aab2e3f6213509ed0ffe5c052f9f6e
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-07 13:50:20 +1100

    Full run test - one hour

--- run_tree_mixtures.sh
+++ run_tree_mixtures.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
 #SBATCH --job-name=tree_mixtures
-#SBATCH --output=%j.%x.out
-#SBATCH --error=%j.%x.err 
+#SBATCH --output=/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/%j.%x.out
+#SBATCH --error=/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/%j.%x.err 
 #SBATCH --partition=Standard
 #
-#SBATCH --time=3:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=50
 #SBATCH --mem-per-cpu=100
 #
-##SBATCH --mail-user u5348329@anu.edu.au
-##SBATCH --mail-type ALL
+#SBATCH --mail-user u5348329@anu.edu.au
+#SBATCH --mail-type ALL
 
 # Change to working directory for this project
-cd /home/u5348329/metazoan-mixtures/
+cd /mnt/data/dayhoff/home/u5348329/metazoan-mixtures/
 
 # # Activate Anaconda work environment
 # source /home/u5348329/.bashrc
