6a3d0a92ed67c29217d91bb2b0f18bd142241d0d
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-07 13:29:04 +1100

    Write and save the number of parallel threads used

--- code/01_estimate_all_trees_parallel.R
+++ code/01_estimate_all_trees_parallel.R
@@ -72,7 +72,7 @@ if (location == "local"){
 }
 
 input_args = commandArgs(trailingOnly = TRUE)
-print(input_args)
+print(paste0("Number of parallel processes to run: ", input_args))
 if (length(input_args)==0) {
   stop("One argument must be supplied (number of parallel threads)", call.=TRUE)
 }else if (length(input_args) > 0){
