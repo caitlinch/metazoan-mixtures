ecb27763e16d1da11d38ce54e2d1020d88dff909
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-11 13:40:53 +1100

    Updated params for dayhoff runs

--- slurm_scripts/job_file_template.sh
+++ slurm_scripts/job_file_template.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-#SBATCH --job-name=tree_mixtures.${id}
-#SBATCH --output=/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/%j.${id}.%x.out
-#SBATCH --error=/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/%j.${id}.%x.err 
+#SBATCH --job-name=tm.${id}
+#SBATCH --output=/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/job_files/%j.%x.out
+#SBATCH --error=/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/job_files/%j.%x.err 
 #SBATCH --partition=Standard
 #
-#SBATCH --time=72:00:00 # 72 hours = 3 days
+#SBATCH --time=72:00:00 # 72 hours
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=7000 # 7GB (largest alignment run so far needed 6.5 GB)
+#SBATCH --mem-per-cpu=7000 # 7GB
 #
 #SBATCH --mail-user u5348329@anu.edu.au
 #SBATCH --mail-type NONE
