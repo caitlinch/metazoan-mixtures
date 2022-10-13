9c785c9db9eab7bca8cc12b612a95990813ccdd6
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-10-13 11:27:08 +1100

    New file for plots and data analysis

---
+++ code/02_data_analysis_and_plotting.R
@@ -0,0 +1,31 @@
+# metazoan-mixtures/code/02_data_analysis_and_plotting.R
+## This script performs data analysis and creates plots of methods and results
+# Caitlin Cherryh, 2022
+
+## This script:
+# 1. Plots figures for the introduction and methods section of the manuscript
+
+
+
+#### 1. Input parameters ####
+## Specify parameters:
+# output_dir          <- Directory for IQ-Tree output (trees and tree mixtures)
+# results_dir         <- Directory for results and plots
+# repo_dir            <- Location of caitlinch/metazoan-mixtures github repository
+
+output_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/"
+repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
+
+
+
+#### 2. Prepare functions, variables and packages ####
+# Open packages
+library(ggplot2)
+library(ggtree)
+library(patchwork)
+
+
+
+#### 3. Plot figures for introduction and methods sections of manuscript ####
+
+
