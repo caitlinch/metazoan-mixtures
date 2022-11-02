2b6a6e0c753ed502af1c13e532e45052b07f722e
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-11-02 15:48:44 +1100

    Philippe 2011 taxa converted

--- code/00_standardise_names.R
+++ code/00_standardise_names.R
@@ -126,13 +126,14 @@ manual_taxonomy_df$original_name <- gsub("\\_\\_", "", manual_taxonomy_df$origin
 manual_taxonomy_df$original_name <- gsub("_$","",manual_taxonomy_df$original_name)
 
 # For each taxa in the mastmet_df, find a consistent taxa name in the Li tsv files
-relabelled_taxa <- lapply(1:nrow(mastmet_df), function(i){find.species.name(mastmet_df[i,], taxon_table_df)})
+relabelled_taxa <- lapply(1:nrow(mastmet_df), function(i){find.species.name(mastmet_df[i,], taxon_table_df, manual_taxonomy_df)})
+
+
 laumer2018_inds <- which(mastmet_df$dataset == "Laumer2018")
 laumer2019_inds <- which(mastmet_df$dataset == "Laumer2019")
-philippe2011_inds <- which(mastmet_df$dataset == "Philippe2011") # To fix: rows 671 692 694 from mastmet
 pick2010_inds <- which(mastmet_df$dataset == "Pick2010")
 simion2017_inds <- which(mastmet_df$dataset == "Simion2017")
 species_row <- mastmet_df[1,]
 
-unlist(lapply(which(mastmet_df$dataset == "Philippe2011"), function(i){find.species.name(mastmet_df[i,], taxon_table_df)}))
+unlist(lapply(which(mastmet_df$dataset == "Philippe2011"), function(i){find.species.name(mastmet_df[i,], taxon_table_df, manual_taxonomy_df)}))
 
