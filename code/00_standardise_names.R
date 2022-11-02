7e632281a5410a32653d7f990a9de0db744a9ec7
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-11-02 14:47:12 +1100

    Updated mastmet_df (better alignment names, fixed some typos, updated Nosenko2013 dataset name.

--- code/00_standardise_names.R
+++ code/00_standardise_names.R
@@ -38,22 +38,22 @@ source(paste0(repo_dir, "code/data_dataset_info.R"))
 
 # Remove the individual dataset lists (only need collated lists) (yes it is a bit cheeky to hard code the removal)
 rm(borowiec2015_list, chang2015_list, dunn2008_list, hejnol2009_list, laumer2018_list, laumer2019_list, moroz2014_list, nosenko2013_list, philippe2009_list,
-     philippe2011_list, pick2010_list, ryan2013_list, simion2017_list, whelan2015_list, whelan2017_list, all_models)
+     philippe2011_list, pick2010_list, ryan2013_list, simion2017_list, whelan2015_list, whelan2017_list, all_models, models_list)
 
 #### 3. Prepare name csv ####
 # Set a file path for the MAST metazoan taxa collation csv
 mastmet_file_path <- paste0(output_dir, "MAST_metazoa_taxa_collation.csv")
 if (file.exists(mastmet_file_path) == FALSE){
   # Create a new data frame with all the taxa from all the matrices you're using
-  mastmet_df <- data.frame("dataset" = c(rep("Borowiec0215", extract.taxa.vector(all_datasets[["Borowiec2015"]])$number),
+  mastmet_df <- data.frame("dataset" = c(rep("Borowiec2015", extract.taxa.vector(all_datasets[["Borowiec2015"]])$number),
                                          rep("Chang2015", extract.taxa.vector(all_datasets[["Chang2015"]])$number),
                                          rep("Dunn2008", extract.taxa.vector(all_datasets[["Dunn2008"]])$number),
                                          rep("Hejnol2009", extract.taxa.vector(all_datasets[["Hejnol2009"]])$number),
                                          rep("Laumer2018", extract.taxa.vector(filter.matrix.names(all_datasets[["Laumer2018"]], matrix_taxa[["Laumer2018.Tplx_phylo_d1.aa"]]))$number),
                                          rep("Laumer2019", extract.taxa.vector(filter.matrix.names(all_datasets[["Laumer2019"]], matrix_taxa[["Laumer2019.nonbilateria_MARE_BMGE.aa"]]))$number),
                                          rep("Moroz2014", extract.taxa.vector(filter.matrix.names(all_datasets[["Moroz2014"]], matrix_taxa[["Moroz2014.ED3d.aa"]]))$number),
-                                         rep("Nosenko2013.nonribosomal", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.nonribosomal_9187_smatrix.aa"]]))$number),
-                                         rep("Nosenko2013.ribosomal", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.ribosomal_14615_smatrix.aa"]]))$number),
+                                         rep("Nosenko2013", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.nonribosomal_9187_smatrix.aa"]]))$number),
+                                         rep("Nosenko2013", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.ribosomal_14615_smatrix.aa"]]))$number),
                                          rep("Philippe2009", extract.taxa.vector(all_datasets[["Philippe2009"]])$number),
                                          rep("Philippe2011", extract.taxa.vector(all_datasets[["Philippe2011"]])$number),
                                          rep("Pick2010", extract.taxa.vector(all_datasets[["Pick2010"]])$number),
@@ -93,22 +93,22 @@ if (file.exists(mastmet_file_path) == FALSE){
                                        extract.taxa.vector(filter.matrix.names(all_datasets[["Simion2017"]], matrix_taxa[["Simion2017.supermatrix_97sp_401632pos_1719genes.aa"]]))$clade,
                                        extract.taxa.vector(filter.matrix.names(all_datasets[["Whelan2015"]], matrix_taxa[["Whelan2015.Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned.aa"]]))$clade,
                                        extract.taxa.vector(all_datasets[["Whelan2017"]])$clade),
-                           "alignment" = c(rep("Borowiec0215.Best108", extract.taxa.vector(all_datasets[["Borowiec2015"]])$number),
-                                           rep("Chang2015.Chang_AA", extract.taxa.vector(all_datasets[["Chang2015"]])$number),
-                                           rep("Dunn2008.Dunn2008_FixedNames", extract.taxa.vector(all_datasets[["Dunn2008"]])$number),
-                                           rep("Hejnol2009.Hejnol_etal_2009_FixedNames", extract.taxa.vector(all_datasets[["Hejnol2009"]])$number),
-                                           rep("Laumer2018.Tplx_phylo_d1", extract.taxa.vector(filter.matrix.names(all_datasets[["Laumer2018"]], matrix_taxa[["Laumer2018.Tplx_phylo_d1.aa"]]))$number),
-                                           rep("Laumer2019.nonbilateria_MARE_BMGE", extract.taxa.vector(filter.matrix.names(all_datasets[["Laumer2019"]], matrix_taxa[["Laumer2019.nonbilateria_MARE_BMGE.aa"]]))$number),
-                                           rep("Moroz2014.ED3d", extract.taxa.vector(filter.matrix.names(all_datasets[["Moroz2014"]], matrix_taxa[["Moroz2014.ED3d.aa"]]))$number),
-                                           rep("Nosenko2013.nonribosomal_9187_smatrix", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.nonribosomal_9187_smatrix.aa"]]))$number),
-                                           rep("Nosenko2013.ribosomal_14615_smatrix", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.ribosomal_14615_smatrix.aa"]]))$number),
-                                           rep("Philippe2009.Philippe_etal_superalignment_FixedNames", extract.taxa.vector(all_datasets[["Philippe2009"]])$number),
-                                           rep("Philippe2011.UPDUNN_MB_FixedNames", extract.taxa.vector(all_datasets[["Philippe2011"]])$number),
-                                           rep("Pick2010.Pick2010", extract.taxa.vector(all_datasets[["Pick2010"]])$number),
-                                           rep("Ryan2013.REA_EST_includingXenoturbella", extract.taxa.vector(all_datasets[["Ryan2013"]])$number),
-                                           rep("Simion2017.supermatrix_97sp_401632pos_1719genes", extract.taxa.vector(filter.matrix.names(all_datasets[["Simion2017"]], matrix_taxa[["Simion2017.supermatrix_97sp_401632pos_1719genes.aa"]]))$number),
-                                           rep("Whelan2015.Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned", extract.taxa.vector(filter.matrix.names(all_datasets[["Whelan2015"]], matrix_taxa[["Whelan2015.Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned.aa"]]))$number),
-                                           rep("Whelan2017.Metazoa_Choano_RCFV_strict", extract.taxa.vector(all_datasets[["Whelan2017"]])$number) ) )
+                           "alignment" = c(rep("Best108", extract.taxa.vector(all_datasets[["Borowiec2015"]])$number),
+                                           rep("Chang_AA", extract.taxa.vector(all_datasets[["Chang2015"]])$number),
+                                           rep("Dunn2008_FixedNames", extract.taxa.vector(all_datasets[["Dunn2008"]])$number),
+                                           rep("Hejnol_etal_2009_FixedNames", extract.taxa.vector(all_datasets[["Hejnol2009"]])$number),
+                                           rep("Tplx_phylo_d1", extract.taxa.vector(filter.matrix.names(all_datasets[["Laumer2018"]], matrix_taxa[["Laumer2018.Tplx_phylo_d1.aa"]]))$number),
+                                           rep("nonbilateria_MARE_BMGE", extract.taxa.vector(filter.matrix.names(all_datasets[["Laumer2019"]], matrix_taxa[["Laumer2019.nonbilateria_MARE_BMGE.aa"]]))$number),
+                                           rep("ED3d", extract.taxa.vector(filter.matrix.names(all_datasets[["Moroz2014"]], matrix_taxa[["Moroz2014.ED3d.aa"]]))$number),
+                                           rep("nonribosomal_9187_smatrix", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.nonribosomal_9187_smatrix.aa"]]))$number),
+                                           rep("ribosomal_14615_smatrix", extract.taxa.vector(filter.matrix.names(all_datasets[["Nosenko2013"]], matrix_taxa[["Nosenko2013.ribosomal_14615_smatrix.aa"]]))$number),
+                                           rep("Philippe_etal_superalignment_FixedNames", extract.taxa.vector(all_datasets[["Philippe2009"]])$number),
+                                           rep("UPDUNN_MB_FixedNames", extract.taxa.vector(all_datasets[["Philippe2011"]])$number),
+                                           rep("Pick2010", extract.taxa.vector(all_datasets[["Pick2010"]])$number),
+                                           rep("REA_EST_includingXenoturbella", extract.taxa.vector(all_datasets[["Ryan2013"]])$number),
+                                           rep("supermatrix_97sp_401632pos_1719genes", extract.taxa.vector(filter.matrix.names(all_datasets[["Simion2017"]], matrix_taxa[["Simion2017.supermatrix_97sp_401632pos_1719genes.aa"]]))$number),
+                                           rep("Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned", extract.taxa.vector(filter.matrix.names(all_datasets[["Whelan2015"]], matrix_taxa[["Whelan2015.Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned.aa"]]))$number),
+                                           rep("Metazoa_Choano_RCFV_strict", extract.taxa.vector(all_datasets[["Whelan2017"]])$number) ) )
   # Save the data frame as a csv file
   write.csv(mastmet_df,  file = mastmet_file_path, row.names = F)
 } else if (file.exists(mastmet_file_path) == TRUE){
@@ -118,8 +118,10 @@ if (file.exists(mastmet_file_path) == FALSE){
 
 #### 4. Standardize the names ####
 # Open the tsv files from Li et. al. 2021
-taxtab_df <- read.delim(taxon_table_path)
-mantax_df <- read.delim(manual_taxonomy_path)
-# For each taxa in the mastmet_df, find a consistent taxa name in the Li tsvs
+taxon_table_df <- read.delim(taxon_table_path)
+manual_taxonomy_df <- read.delim(manual_taxonomy_path)
+# For each taxa in the mastmet_df, find a consistent taxa name in the Li tsv files
+s <- mastmet_df$original_name[1]
+species_row <- mastmet_df[1,]
 
 
