843de0fd92ee1e1110091e911445aff2ffe37ca7
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-11-09 16:57:03 +1100

    New file to update tips in trees

---
+++ code/02_maximum_likelihood_tree_processing.R
@@ -0,0 +1,32 @@
+# metazoan-mixtures/code/02_maximum_likelihood_tree_processing.R
+## This script processes maximum likelihood trees (so all trees from all datasets are consistent)
+# Caitlin Cherryh, 2022
+
+## This script:
+# 1. Plots figures for the introduction and methods section of the manuscript
+
+
+
+#### 1. Input parameters ####
+## Specify parameters:
+# output_dir          <- Directory for IQ-Tree output (.log, .iqtree and .treefile files from IQ-Tree runs)
+# results_dir         <- Directory for results and plots
+# repo_dir            <- Location of caitlinch/metazoan-mixtures github repository
+
+output_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/02_maximum_likelihood_trees/"
+results_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/"
+repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
+
+
+
+#### 2. Prepare functions, variables and packages ####
+# Open packages
+library(ape)
+
+
+
+#### 3. Plot figures for introduction and methods sections of manuscript ####
+## Plotting the alternative phylogenetic hypotheses
+# Open the trees
+trees_file <- paste0(repo_dir, "trees/alternative_phylogenetic_hypotheses.nex")
+trees <- read.tree(trees_file)
