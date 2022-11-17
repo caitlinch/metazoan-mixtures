8e37b551e8910be32cc3fb809dd0f9aada3ce23f
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-11-17 16:26:40 +1100

    Test params for updating tips function

--- code/02_maximum_likelihood_tree_processing.R
+++ code/02_maximum_likelihood_tree_processing.R
@@ -9,13 +9,13 @@
 
 #### 1. Input parameters ####
 ## Specify parameters:
-# output_dir          <- Directory for IQ-Tree output (.log, .iqtree and .treefile files from IQ-Tree runs)
-# results_dir         <- Directory for results and plots
-# repo_dir            <- Location of caitlinch/metazoan-mixtures github repository
+# output_dir    <- Directory for IQ-Tree output (.log, .iqtree and .treefile files from IQ-Tree runs)
+# results_dir   <- Directory for results and plots
+# repo_dir      <- Location of caitlinch/metazoan-mixtures github repository
 
-output_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/02_maximum_likelihood_trees/"
-results_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/"
-repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
+output_dir      <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/02_maximum_likelihood_trees/01_ml_tree_output_files/"
+results_dir     <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/02_maximum_likelihood_trees/02_renamed_trees/"
+repo_dir        <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
 
 
 
@@ -23,10 +23,18 @@ repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
 # Open packages
 library(ape)
 
+# Source functions and taxa lists
+source(paste0(repo_dir, "code/func_naming.R"))
 
+# Open the renaming csv
+naming_reconciliation_df <- read.csv(paste0(repo_dir, "MAST_metazoa_taxa_reconciliation.csv"), stringsAsFactors = FALSE)
 
 #### 3. Update the taxa labels in each tree ####
 # Extract the full list of trees
-all_files <- list.files(output_dir)
-all_trees <- grep(".treefile", all_files, value = T)
+all_files <- paste0(output_dir, list.files(output_dir))
+all_tree_files <- grep("\\.treefile", all_files, value = T)
+
+treefile <- all_tree_files[1]
+output.directory = results_dir
+save.updated.tree = TRUE
 
