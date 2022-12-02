8fe83369d2452d0677a70beb762412b907cd12dc
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-12-02 14:19:48 +1100

    New file for PMSF/CAT-PMSF functions

---
+++ code/func_pmsf_trees.R
@@ -0,0 +1,5 @@
+## caitlinch/metazoan-mixtures/func_pmsf_trees.R
+# Caitlin Cherryh 2022
+
+# Functions for estimating maximum likelihood trees and constrained maximum likelihood trees with PMSF and CAT-PMSF models
+
