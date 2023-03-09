65017b1564f0211ef4bae825f3bd08da7eede053
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-03-09 18:18:49 +1100

    Update date.

--- code/util_identify_extract_unrun_combinations_ML_trees.R
+++ code/util_identify_extract_unrun_combinations_ML_trees.R
@@ -12,5 +12,5 @@ ids_to_run <- df$prefix[inds_to_run]
 files_to_run <- paste0("iqtree_run_", inds_to_run, ".sh")
 df2 <- data.frame(index = inds_to_run, prefix = ids_to_run, sbatch_file = files_to_run)
 # Save output dataframe
-df2_file_path <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/output/missing_runs_20230301.csv"
+df2_file_path <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/output/missing_runs_20230309.csv"
 write.csv(df2, file = df2_file_path, row.names = F)
