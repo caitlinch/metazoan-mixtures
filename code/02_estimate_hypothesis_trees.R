a835693bcb8184b40972a2155ddc4082e20fe31d
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-05-26 12:52:32 +1000

    Remove unneeded file paths and remove formatting of models. Add new output file paths.

--- code/02_estimate_hypothesis_trees.R
+++ code/02_estimate_hypothesis_trees.R
@@ -9,14 +9,12 @@
 # ml_tree_dir                 <- Directory containing all maximum likelihood trees estimating in pipeline step 01
 # output_dir                  <- Directory for IQ-Tree output (trees and tree mixtures)
 # repo_dir                    <- Location of caitlinch/metazoan-mixtures github repository
-
+# number_parallel_processes   <- The number of simultaneous processes to run at once using mclapply(). 
+#                                     If 1, then all processes will run sequentially
 # iqtree2                     <- Location of IQ-Tree2 stable release
 # iqtree_num_threads          <- Number of parallel threads for IQ-Tree to use. Can be a set number (e.g. 2) or "AUTO"
 #                                     See IQ-Tree documentation for more details (http://www.iqtree.org/doc/Command-Reference)
 # hypothesis_tree_bootstraps  <- Number of ultrafast bootstraps (UFB) to perform in IQ-Tree when estimating constrained maximum likelihood trees
-# number_parallel_processes   <- The number of simultaneous processes to run at once using mclapply(). 
-#                                     If 1, then all processes will run sequentially
-
 
 ## Specify control parameters (all take logical values TRUE or FALSE:
 # extract.ML.tree.information   <- TRUE to extract information from maximum likelihood tree log file and iqtree file, including tree topology. FALSE to skip.
@@ -29,14 +27,14 @@ if (location == "local"){
   ml_tree_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/01_Data_all/"
   output_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/"
   repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
-  iqtree2 <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/03_Software_IQ-Tree/iqtree-2.2.0-MacOSX/bin/iqtree2"
   number_parallel_processes <- 1
+  iqtree2 <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/03_Software_IQ-Tree/iqtree-2.2.0-MacOSX/bin/iqtree2"
 } else if (location == "dayhoff"){
   ml_tree_dir <- ""
   output_dir <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/output/"
   repo_dir <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/"
-  iqtree2 <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/iqtree/iqtree-2.2.0-Linux/bin/iqtree2"
   number_parallel_processes <- 4
+  iqtree2 <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/iqtree/iqtree-2.2.0-Linux/bin/iqtree2"
 } 
 
 # Set parameters that are identical for all run locations
@@ -70,47 +68,17 @@ rm(borowiec2015_list, chang2015_list, dunn2008_list, hejnol2009_list, laumer2018
 
 
 #### 2. Prepare variables ####
-# Extract the list of all files from the folder containing alignments/models/partition files
-all_files <- list.files(alignment_dir)
-if (length(all_files) > 0){
-  all_files <- paste0(alignment_dir, all_files)
-}
-# Extract the list of alignments (i.e. files that contain the word "alignment")
-all_alignments <- grep("\\.alignment\\.", all_files, value = T)
-
-# Sort and categorise models of sequence evolution 
-all_models <- sort(unique(unlist(lapply(all_models, sort.model.chunks)))) 
-# Identify unique matrices within the models
-model_components <- sort(unique(unlist(strsplit(all_models, "\\+"))))
-# Remove components that do not correspond to a matrix or model (e.g. remove "I", "F", "G4")
-model_components <- model_components[!model_components %in% c("F", "FO", "G", "G4", "I", "R", "R4")]
-# Create a vector of models to exclude from analysis
-#   PMSF: PMSF is more complex to run than other models - requires separate function/multiple steps (see separate PMSF file)
-#   CAT: CAT is not a model in IQ-Tree. Use C10:C60 instead.
-#   GTR: GTR is a DNA model
-#   F81: F81 is a DNA model
-exclude_models <- c("PMSF", "CAT", "GTR", "F81", "C10", "C30", "C40", "C50")
-# Remove the models to exclude from the list of models
-model_components <- model_components[which(!model_components %in% exclude_models)]
-# Add the newer models 
-model_components <- c(model_components, "'LG+C20'", "'LG+C60'")
-# Sort the mode components
-model_components <- sort(model_components)
-# Move ModelFinder to the end of the list (will take the longest as has to test all possible models, so run last)
-model_components <- model_components[!model_components == "ModelFinder"]
-model_components <- c(model_components, "ModelFinder")
-# Note: partitioning schemes currently not possible in mixture of trees implementation
-
 # Create output folders
-# Create a folder for the ml trees
-ml_tree_dir <- paste0(output_dir, "maximum_likelihood_trees/")
-if (file.exists(ml_tree_dir) == FALSE){dir.create(ml_tree_dir)}
 # Create a folder for the constraint trees
 c_tree_dir <- paste0(output_dir, "constraint_trees/")
 if (file.exists(c_tree_dir) == FALSE){dir.create(c_tree_dir)}
+# Create a folder for the hypothesis trees
+h_tree_dir <- paste0(output_dir, "hypothesis_trees/")
+if (file.exists(h_tree_dir) == FALSE){dir.create(h_tree_dir)}
 
 # Create file paths for output files
-ml_run_output_path <- paste0(output_dir, "01_02_maximum_likelihood_results.tsv")
+ml_tree_df_file             <- paste0(repo_dir, "output/01_01_maximum_likelihood_tree_estimation_parameters.tsv")
+ml_extracted_output_path    <- paste0(repo_dir, "output/01_02_maximum_likelihood_results.tsv")
 
 
 
@@ -118,7 +86,7 @@ ml_run_output_path <- paste0(output_dir, "01_02_maximum_likelihood_results.tsv")
 # Extract information about each run from the IQ-Tree output and log files
 if (extract.ML.tree.information == TRUE){
   # Open ml_tree_df file 
-  ml_tree_df <- read.table(df_op_01_01, header = T)
+  ml_tree_df <- read.table(ml_tree_df_file, header = T)
   
   # Make a list of .iqtree files (and .log files)
   all_iqtree_files <- paste0(ml_tree_dir, ml_tree_df$iqtree_file)
