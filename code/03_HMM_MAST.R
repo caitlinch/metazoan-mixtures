aeca8345d6aded37155d2eb87f00bed4a2bb1db0
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-06-01 16:25:27 +1000

    Prepare df for phyloHMM run

--- code/03_HMM_MAST.R
+++ code/03_HMM_MAST.R
@@ -15,6 +15,7 @@
 #                               Alignment naming convention: [manuscript].[matrix_name].[sequence_type].fa
 #                               E.g. Cherryh2022.alignment1.aa.fa
 # hypothesis_tree_dir     <- Directory containing constrained maximum likelihood trees
+# pmsf_sitefreq_dir       <- Directory containing output files (including .sitefreq files) from IQ-Tree runs with PMSF model
 # mast_dir                <- Directory for phyloHMM/MAST output
 # repo_dir                <- Location of caitlinch/metazoan-mixtures github repository
 
@@ -26,6 +27,7 @@ location = "local"
 if (location == "local"){
   alignment_dir           <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/01_Data_all/"
   hypothesis_tree_dir     <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/04_hypothesis_trees/"
+  pmsf_sitefreq_dir       <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/02_pmsf_site_freqs/"
   mast_dir                <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/05_tree_mixtures/"
   repo_dir                <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
   
@@ -37,18 +39,37 @@ if (location == "local"){
 
 
 #### 2. Prepare variables, open packages and source functions ####
-test_trees_file <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/constraint_trees/00_test_phyloHMM/Test.Nosenko2013.nonribosomal_9187_smatrix.LG.ML_Hypothesis_trees.treefile"
-tree_file <- test_trees_file
-alignment_file <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/01_Data_all/Nosenko2013.nonribosomal_9187_smatrix.aa.alignment.phy"
-output_prefix <- "test.Nosenko2013.nonribo.LG.HMM"
-model <- "LG"
-MAST_model <- paste0(model, "+TR") # branch-length restricted MAST model: where a branch occurs in multiple treesm it is constrained to have the same length in each tree
 
-# To run phyloHMM for the toy example
-iqtree_hmm_command <- run.phyloHMM(tree_file = tree_file, alignment_file = alignment_file, MAST_model = MAST_model, output_prefix = output_prefix, 
-                                   iqtree_phyloHMM = iqtree_tm, iqtree_num_threads = "AUTO", run.iqtree = FALSE)
-# To extract information from the completed HMM run:
-hmm_output <- extract.phyloHMM.output(output_prefix = output_prefix, output_directory = "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/constraint_trees/00_test_phyloHMM/")
+
+
+
+#### 3. Prepare for analysis ####
+## Prepare the dataframe 
+model_df <- read.table(paste0(repo_dir, "output/01_03_best_models_per_alignment.tsv"), header = T)
+# Add the alignments to the dataframe
+all_aldir_files <- list.files(alignment_dir)
+al_files <- grep("alignment", grep("00_", all_aldir_files, value = T, invert = T), value = T)
+model_df$alignment_path <- unlist(lapply(1:nrow(model_df), function(i){
+  grep(model_df$dataset[i], grep(model_df$matrix_name[i], al_files, value = T), value = T)
+  }))
+# Split the best_model column into two columns: one for the best model to input into IQ-Tree, 
+#     and one for the path to the .sitefreq file (present only if best model is a PMSF model)
+split_best_model_str                <- strsplit(model_df$best_model, ":")
+model_df$best_model                 <- unlist(lapply(1:nrow(model_df), function(x){split_best_model_str[[x]][1]}))
+model_df$best_model_sitefreq_path   <- unlist(lapply(1:nrow(model_df), function(x){split_best_model_str[[x]][2]}))
+# Reorder the columns
+model_df <- model_df[,c("dataset", "model_code", "matrix_name", "alignment_path", "sequence_format", "prefix",
+                        "best_model", "best_model_sitefreq_path", "best_model_LogL", "best_model_BIC",
+                        "best_model_wBIC", "tree_LogL", "tree_UnconstrainedLogL", "tree_NumFreeParams",
+                        "tree_BIC", "tree_length", "tree_SumInternalBranch", "tree_PercentInternalBranch",
+                        "estimated_rates", "estimated_gamma", "estimated_state_frequencies",
+                        "maximum_likelihood_tree")]
+# Add the  file paths to the dataframe
+model_df$best_model_sitefreq_path <- paste0(pmsf_sitefreq_dir, model_df$best_model_sitefreq_path)
+model_df$alignment_path <- paste0(alignment_dir, model_df$alignment_path)
+
+# Prepare the hypothesis tree files
+
 
 
 
