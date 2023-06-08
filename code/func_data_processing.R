cf1c38907795d5ea35dfdde4b1c590e0893dfcc1
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-06-08 12:24:52 +1000

    New function for formatting AU test results.

--- code/func_data_processing.R
+++ code/func_data_processing.R
@@ -1388,3 +1388,26 @@ check.remaining.runs <- function(dataset, input_parameter_file, output_parameter
 }
 
 
+
+
+#### Summarise information from output csvs ####
+summarise.AU.test.results <- function(dataset_id, au_test_df){
+  ## Function to take one dataset and summarise the AU test results in one row
+  
+  # Get rows of dataframe that have matching ID
+  d_df <- au_test_df[au_test_df$ID == dataset_id,]
+  # Extract year of publication
+  dataset_year <- as.numeric(str_extract(unique(d_df$dataset), "(\\d)+"))
+  # Process the dataset df (depending on the number of trees)
+  if (nrow(d_df) == 3){
+    au_test_pvalues <- c(d_df$p_AU, NA, NA)
+  } else if (nrow(d_df) == 5){
+    au_test_pvalues <- d_df$p_AU
+  }
+  # Create a new row for output
+  new_row <- c(d_df$dataset[1], d_df$matrix[1], d_df$best_model_code[1], "AU_test_p_values", au_test_pvalues, dataset_year)
+  names(new_row) <- c("dataset", "matrix", "best_model_code", "topology_test", "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")
+  # Return the output
+  return(new_row)
+}
+
