d8b0ee6eb4ac2a644911b59cb10f35375b5d9042
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-06-23 13:55:38 +1000

    Reduce code by placing repeated plots inside functions and just calling functions in body of script.

--- code/04_data_analysis_and_plotting.R
+++ code/04_data_analysis_and_plotting.R
@@ -29,7 +29,7 @@ source(paste0(repo_dir,"code/func_plotting.R"))
 
 
 #### 3. Plot figures for introduction and methods sections of manuscript ####
-## Plotting the alternative phylogenetic hypotheses ##
+### Plotting the alternative phylogenetic hypotheses ###
 # Open the trees
 trees_file <- paste0(repo_dir, "trees/alternative_phylogenetic_hypotheses.nex")
 trees <- read.tree(trees_file)
@@ -41,126 +41,42 @@ metazoan_clade_labs <- data.frame("clade" = c("Bilateria", "Cnidaria", "Ctenopho
 metazoan_clade_labs$lab <- metazoan_clade_labs$clade
 metazoan_clade_labs$html_lab <- paste0("<b style='color:", metazoan_clade_labs$color, "'>", metazoan_clade_labs$clade, "</b>")
 
-### Black and White
+### Black and White plots ###
 # Plot one tree at a time
-hyp1_plot <- ggtree(trees[[1]]) + geom_tree() + 
-  geom_tiplab(geom = "text", size = 10) +
-  theme_tree2(plot.margin=margin(6, 145, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white"))
-
-hyp2_plot <- ggtree(trees[[2]]) + geom_tree() + 
-  geom_tiplab(geom = "text", size = 10) +
-  theme_tree2(plot.margin=margin(6, 145, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white"))
-
-hyp3_plot <- ggtree(trees[[3]]) + geom_tree() + 
-  geom_tiplab(geom = "text", size = 10) +
-  theme_tree2(plot.margin=margin(6, 145, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white"))
-
-hyp4_plot <- ggtree(trees[4]) + geom_tree() +
-  geom_tiplab(geom = "text", size = 8) +
-  theme_tree2(plot.margin=margin(6, 70, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  geom_cladelab(node=12, label="Porifera", textcolor="grey50", barcolor = "grey50", offset=7, geom = "text", align=TRUE, fontsize = 7, hjust = -0.1) +
-  geom_cladelab(node=14, label="Porifera", textcolor="grey50", barcolor = "grey50", offset=7, geom = "text", align=TRUE, fontsize = 7, hjust = -0.1) +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white"))
-
-
-hyp5_plot <- ggtree(trees[[5]]) + geom_tree() + 
-  geom_tiplab(geom = "text", size = 8) +
-  theme_tree2(plot.margin=margin(6, 70, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  geom_cladelab(node=11, label="Porifera", textcolor="grey50", barcolor = "grey50", offset=7, geom = "text", align=TRUE, fontsize = 7, hjust = -0.1) +
-  geom_cladelab(node=13, label="Porifera", textcolor="grey50", barcolor = "grey50", offset=7, geom = "text", align=TRUE, fontsize = 7, hjust = -0.1) +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white"))
-
+hyp1_plot <- bw.monophyletic.clades.plot(trees[[1]])
+hyp2_plot <- bw.monophyletic.clades.plot(trees[[2]])
+hyp3_plot <- bw.monophyletic.clades.plot(trees[[3]])
+hyp4_plot <- bw.paraphyletic.clades.plot(trees[[4]], label_nodes = c(12, 14))
+hyp5_plot <- bw.paraphyletic.clades.plot(trees[[5]], label_nodes = c(11, 13))
 # Combine the four hypothesis trees into one 
 patchwork_hyps <- (hyp1_plot | hyp2_plot | hyp3_plot)/(hyp4_plot | hyp5_plot) + 
   plot_annotation(tag_levels = "a", tag_suffix = ".") & theme(plot.tag = element_text(size = 20))
-
 # Export hypothesis plots 
 hypothesis_plot_file <- paste0(output_dir, "hypothesis_tree_example_plot.png")
 png(filename = hypothesis_plot_file, width = 1090, height = 723, units = "px", pointsize = 12, bg = "white")
 patchwork_hyps
 dev.off()
 
-### Colour
-# Colorblind friendly palette with grey:
+### Colour plots ###
+# Colourblind friendly palette with grey:
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+metazoan_palette <- c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999")
 
 # Plot one tree at a time
-hyp1_plot <- ggtree(trees[[1]]) %<+% metazoan_clade_labs +
-  geom_tree(size = 1.5) +
-  geom_tiplab(aes(label = lab, color = color), parse = TRUE, show.legend = FALSE, geom = "text", size = 10, fontface = 2) +
-  theme_tree2(plot.margin=margin(6, 145, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white")) +
-  scale_color_manual(values = c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999"))
+hyp1_plot <- color.clades.plot(trees[[1]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
+                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_1_plot_color")
+hyp2_plot <- color.clades.plot(trees[[2]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
+                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_2_plot_color")
+hyp3_plot <- color.clades.plot(trees[[3]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
+                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_3_plot_color")
+hyp4_plot <- color.clades.plot(trees[[4]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
+                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_4_plot_color")
+hyp5_plot <- color.clades.plot(trees[[5]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
+                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_5_plot_color")
 
 
-hyp2_plot <- ggtree(trees[[2]]) %<+% metazoan_clade_labs +
-  geom_tree(size = 1.5) +
-  geom_tiplab(aes(label = lab, color = color), parse = TRUE, show.legend = FALSE, geom = "text", size = 10, fontface = 2) +
-  theme_tree2(plot.margin=margin(6, 145, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white")) +
-  scale_color_manual(values = c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999"))
 
-hyp3_plot <- ggtree(trees[[3]]) %<+% metazoan_clade_labs +
-  geom_tree(size = 1.5) +
-  geom_tiplab(aes(label = lab, color = color), parse = TRUE, show.legend = FALSE, geom = "text", size = 10, fontface = 2) +
-  theme_tree2(plot.margin=margin(6, 145, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white")) +
-  scale_color_manual(values = c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999"))
+#### 4. Exploratory plots ####
 
-hyp4_plot <- ggtree(trees[[4]]) %<+% metazoan_clade_labs +
-  geom_tree(size = 1.5) +
-  geom_tiplab(aes(label = lab, color = color), parse = TRUE, show.legend = FALSE, geom = "text", size = 10, fontface = 2) +
-  theme_tree2(plot.margin=margin(6, 250, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white")) +
-  scale_color_manual(values = c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999"))
-
-
-hyp5_plot <- ggtree(trees[[5]]) %<+% metazoan_clade_labs +
-  geom_tree(size = 1.5) +
-  geom_tiplab(aes(label = lab, color = color), parse = TRUE, show.legend = FALSE, geom = "text", size = 10, fontface = 2) +
-  theme_tree2(plot.margin=margin(6, 250, 6, 6)) +
-  coord_cartesian(clip = "off") +
-  theme(axis.text.x = element_text(color = "white"), axis.ticks.x = element_line(color = "white"), axis.line.x = element_line(color = "white")) +
-  scale_color_manual(values = c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999"))
-
-plot_list <- list(hyp1_plot, hyp2_plot, hyp3_plot, hyp4_plot, hyp5_plot)
-
-# Output plot
-for (i in 1:3){
-  hypothesis_plot_file <- paste0(output_dir, "hypothesis_tree_", i, "_plot_color.png")
-  png(filename = hypothesis_plot_file, width = 974, height = 723, units = "px", pointsize = 12, bg = "white")
-  plot_list[[i]]
-  dev.off()
-  
-  hypothesis_plot_file <- paste0(output_dir, "hypothesis_tree_", i, "_plot_color.svg")
-  svg(filename = hypothesis_plot_file, width = 5, height = 3.5, bg = "white")
-  plot_list[[i]]
-  dev.off()
-}
-
-for (i in 4:5){
-  # Make plots longer due to the extra substitution event
-  hypothesis_plot_file <- paste0(output_dir, "hypothesis_tree_", i, "_plot_color.png")
-  png(filename = hypothesis_plot_file, width = 1364, height = 723, units = "px", pointsize = 12, bg = "white")
-  plot_list[[i]]
-  dev.off()
-  
-  hypothesis_plot_file <- paste0(output_dir, "hypothesis_tree_", i, "_plot_color.svg")
-  svg(filename = hypothesis_plot_file, width = 7, height = 3.5, bg = "white")
-  plot_list[[i]]
-  dev.off()
-}
 
 
