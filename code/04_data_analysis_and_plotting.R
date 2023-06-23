c93e15cdcdbe86182b8475228445183ba5e4dd30
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-06-23 14:24:52 +1000

    Formatting and adding doco.

--- code/04_data_analysis_and_plotting.R
+++ code/04_data_analysis_and_plotting.R
@@ -8,11 +8,12 @@
 
 #### 1. Input parameters ####
 ## Specify parameters:
-# output_dir          <- Directory for IQ-Tree output (trees and tree mixtures)
+# plot_dir          <- Directory for IQ-Tree output (trees and tree mixtures)
 # results_dir         <- Directory for results and plots
 # repo_dir            <- Location of caitlinch/metazoan-mixtures github repository
 
-output_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/05_plotting/"
+plot_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/05_plotting/"
+results_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/01_output_files/"
 repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
 
 
@@ -52,7 +53,7 @@ hyp5_plot <- bw.paraphyletic.clades.plot(trees[[5]], label_nodes = c(11, 13))
 patchwork_hyps <- (hyp1_plot | hyp2_plot | hyp3_plot)/(hyp4_plot | hyp5_plot) + 
   plot_annotation(tag_levels = "a", tag_suffix = ".") & theme(plot.tag = element_text(size = 20))
 # Export hypothesis plots 
-hypothesis_plot_file <- paste0(output_dir, "hypothesis_tree_example_plot.png")
+hypothesis_plot_file <- paste0(plot_dir, "hypothesis_tree_example_plot.png")
 png(filename = hypothesis_plot_file, width = 1090, height = 723, units = "px", pointsize = 12, bg = "white")
 patchwork_hyps
 dev.off()
@@ -64,19 +65,28 @@ metazoan_palette <- c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00"
 
 # Plot one tree at a time
 hyp1_plot <- color.clades.plot(trees[[1]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
-                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_1_plot_color")
+                               save.plot = TRUE, output_directory = plot_dir, output_id = "hypothesis_tree_1_plot_color")
 hyp2_plot <- color.clades.plot(trees[[2]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
-                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_2_plot_color")
+                               save.plot = TRUE, output_directory = plot_dir, output_id = "hypothesis_tree_2_plot_color")
 hyp3_plot <- color.clades.plot(trees[[3]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
-                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_3_plot_color")
+                               save.plot = TRUE, output_directory = plot_dir, output_id = "hypothesis_tree_3_plot_color")
 hyp4_plot <- color.clades.plot(trees[[4]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
-                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_4_plot_color")
+                               save.plot = TRUE, output_directory = plot_dir, output_id = "hypothesis_tree_4_plot_color")
 hyp5_plot <- color.clades.plot(trees[[5]], tip_labels = metazoan_clade_labs, color_palette = metazoan_palette, 
-                               save.plot = TRUE, output_directory = output_dir, output_id = "hypothesis_tree_5_plot_color")
+                               save.plot = TRUE, output_directory = plot_dir, output_id = "hypothesis_tree_5_plot_color")
 
 
 
 #### 4. Exploratory plots ####
+# List all files in the results directory
+all_files <- list.files(results_dir)
+
+### Plot number of sites/number of informative sites against proportion of trees with each topology ###
+
+
+
+
+
 
 
 
