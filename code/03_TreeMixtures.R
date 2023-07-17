11f3c137d3503660a6656f18014d319ef927be0c
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-07-17 16:07:30 +1000

    Replace HMM model with MAST only model.

--- code/03_TreeMixtures.R
+++ code/03_TreeMixtures.R
@@ -17,12 +17,11 @@
 #                               E.g. Cherryh2022.alignment1.aa.fa
 # hypothesis_tree_dir     <- Directory containing constrained maximum likelihood trees
 # pmsf_sitefreq_dir       <- Directory containing output files (including .sitefreq files) from IQ-Tree runs with PMSF model
-# mast_dir                <- Directory for phyloHMM/MAST output
+# mast_dir                <- Directory for MAST output
 # output_dir              <- Directory for output csvs
 # repo_dir                <- Location of caitlinch/metazoan-mixtures github repository
 # iqtree2                 <- Location of IQ-Tree2 stable release (version 2.2.2)
-# iqtree_tm               <- Location of IQ-Tree2 phyloHMM release (currently: version 2.2.0.8.mix.1.hmm)
-# iqtree_hmmster          <- Location of IQ-Tree2 HMMster release (currently: version 2.2.3.hmmster)
+# iqtree_MAST             <- Location of IQ-Tree2 MAST release (currently: version 2.2.3.hmmster)
 
 ## Phylogenetic and IQ-Tree2 parameters
 # iqtree_num_threads      <- Number of parallel threads for IQ-Tree to use. Can be a set number (e.g. 2) or "AUTO"
@@ -42,7 +41,7 @@ if (location == "local"){
   output_dir              <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/01_output_files/"
   repo_dir                <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
   iqtree2                 <- "iqtree2"
-  iqtree_hmmster          <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/03_Software_IQ-Tree/iqtree-2.2.3.hmmster-MacOSX/bin/iqtree"
+  iqtree_MAST             <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/03_Software_IQ-Tree/iqtree-2.2.3.hmmster-MacOSX/bin/iqtree"
   
   ## Phylogenetic and IQ-Tree2 parameters
   iqtree_num_threads      <- 3
@@ -56,7 +55,7 @@ if (location == "local"){
   output_dir              <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/output/"
   repo_dir                <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/"
   iqtree2                 <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/iqtree/iqtree-2.2.0-Linux/bin/iqtree2"
-  iqtree_hmmster          <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/iqtree/iqtree-2.2.3.hmmster-Linux/bin/iqtree2"
+  iqtree_MAST             <- "/mnt/data/dayhoff/home/u5348329/metazoan-mixtures/iqtree/iqtree-2.2.3.hmmster-Linux/bin/iqtree2"
   
   ## Phylogenetic and IQ-Tree2 parameters
   iqtree_num_threads      <- 10
@@ -80,12 +79,12 @@ source(paste0(repo_dir, "code/func_estimate_trees.R"))
 
 #### 3. Prepare for analysis ####
 ## Check whether the dataframe for phyloHMM runs has been created
-phylohmm_parameter_path <- paste0(output_dir, "03_01_phyloHMM_parameters.tsv")
-if (file.exists(phylohmm_parameter_path) == TRUE){
-  model_df <- read.table(phylohmm_parameter_path, header = T)
-} else if (file.exists(phylohmm_parameter_path) == FALSE){
+mast_parameter_path <- paste0(output_dir, "03_01_MAST_parameters.tsv")
+if (file.exists(mast_parameter_path) == TRUE){
+  model_df <- read.table(mast_parameter_path, header = T)
+} else if (file.exists(mast_parameter_path) == FALSE){
   ## Prepare the dataframe 
-  model_df <- read.table(paste0(repo_dir, "output/01_03_best_models_per_alignment.tsv"), header = T)
+  model_df <- read.table(paste0(output_dir, "01_03_best_models_per_alignment.tsv"), header = T)
   # Add the alignments to the dataframe
   all_aldir_files <- list.files(alignment_dir)
   al_files <- grep("alignment", grep("00_", all_aldir_files, value = T, invert = T), value = T)
@@ -97,6 +96,9 @@ if (file.exists(phylohmm_parameter_path) == TRUE){
   split_best_model_str                <- strsplit(model_df$best_model, ":")
   model_df$best_model                 <- unlist(lapply(1:nrow(model_df), function(x){split_best_model_str[[x]][1]}))
   model_df$best_model_sitefreq_path   <- unlist(lapply(1:nrow(model_df), function(x){split_best_model_str[[x]][2]}))
+  
+  ## Remove all models without complete hypothesis trees
+  model_df <- model_df[which(model_df$model_class == "profile"), ]
   
   ## Prepare the hypothesis tree files
   model_df$hypothesis_tree_path <- unlist(lapply(paste0(model_df$dataset, ".", model_df$matrix_name, ".", model_df$model_code), 
@@ -120,7 +122,7 @@ if (file.exists(phylohmm_parameter_path) == TRUE){
     model_df <- model_df[order(model_df$dataset, model_df$matrix_name),]
     # Determine minimum branch length as 1/N for each dataset (where N is the total number of sites), and add it to the model_df as a column
     model_df$num_sites <- al_dims_df$num_sites
-    model_df$min_MAST_bl_from_alignment <- round(1/model_df$num_sites, 5)
+    model_df$min_MAST_bl_from_alignment <- round(100/model_df$num_sites, 5)
     model_df$min_MAST_bl_arbitrary <- 0.00001
   }
   
@@ -132,155 +134,76 @@ if (file.exists(phylohmm_parameter_path) == TRUE){
                            "hypothesis_tree_path", "num_sites", "min_MAST_bl_from_alignment", "min_MAST_bl_arbitrary")]
   
   ## Write the dataframe
-  write.table(model_df, file = phylohmm_parameter_path, sep = "\t")
+  write.table(model_df, file = mast_parameter_path, sep = "\t")
   
 }
 
 
 
 
-#### 4. Apply mixtures across trees and sites (MAST model) - phyloHMM ####
-# Create phyloHMM command lines in IQ-Tree
-phyloHMM_run_list <- lapply(1:nrow(model_df), function(i){phyloHMM.wrapper(i, mast_df = model_df, 
-                                                                           iqtree_tree_mixtures = iqtree_tm,
-                                                                           MAST_branch_length_option = "TR", 
-                                                                           iqtree_num_threads = iqtree_num_threads, 
-                                                                           iqtree_min_branch_length = model_df$min_MAST_bl_from_alignment[i],
-                                                                           run.iqtree = FALSE) } )
-phyloHMM_run_df <- as.data.frame(do.call(rbind, phyloHMM_run_list))
+#### 4. Apply mixtures across trees and sites (MAST model) ####
+# Create MAST command lines in IQ-Tree
+MAST_run_df <- as.data.frame(do.call(rbind, lapply(1:nrow(model_df), function(i){MAST.wrapper(i, mast_df = model_df, 
+                                                                                              iqtree_tree_mixtures = iqtree_MAST, 
+                                                                                              MAST_branch_length_option = "T",
+                                                                                              iqtree_num_threads = iqtree_num_threads,
+                                                                                              run.iqtree = FALSE) }) ) )
 # Bind dataframe
-phyloHMM_df <- cbind(model_df,phyloHMM_run_df)
+MAST_run_df <- cbind(model_df, MAST_run_df)
 # Write dataframe
-phylohmm_call_path <- paste0(output_dir, "03_02_phyloHMM_command_lines.tsv")
-write.table(phyloHMM_df, file = phylohmm_call_path, sep = "\t")
+MAST_call_path <- paste0(output_dir, "03_02_MAST_command_lines.tsv")
+write.table(MAST_run_df, file = MAST_call_path, sep = "\t")
 # Write command lines as text file
-phylohmm_call_text_path <- paste0(output_dir, "03_02_phyloHMM_command_lines.txt")
-write(phyloHMM_df$phyloHMM_iqtree2_command, file = phylohmm_call_text_path)
-# Run phyloHMM
-if (run.MAST == TRUE){
-  # Call IQ-Tree2
-  system(phyloHMM_df$phyloHMM_iqtree2_command)
-}
-
-
-
-#### 5. Apply mixtures across trees and sites (MAST model) - HMMster ####
-# Create HMMster command lines in IQ-Tree
-HMMster_run_list <- lapply(1:nrow(model_df), function(i){HMMster.wrapper(i, mast_df = model_df, 
-                                                                         iqtree_tree_mixtures = iqtree_hmmster, 
-                                                                         MAST_branch_length_option = "T",
-                                                                         iqtree_num_threads = iqtree_num_threads,
-                                                                         iqtree_min_branch_length = model_df$min_MAST_bl_from_alignment[i],
-                                                                         run.iqtree = FALSE) } )
-HMMster_run_df <- as.data.frame(do.call(rbind, HMMster_run_list))
-# Bind dataframe
-HMMster_df <- cbind(model_df,HMMster_run_df)
-# Write dataframe
-HMMster_call_path <- paste0(output_dir, "03_02_HMMster_command_lines.tsv")
-write.table(HMMster_df, file = HMMster_call_path, sep = "\t")
-# Write command lines as text file
-HMMster_call_text_path <- paste0(output_dir, "03_02_HMMster_command_lines.txt")
-write(HMMster_df$HMMster_iqtree2_command, file = HMMster_call_text_path)
+MAST_call_text_path <- paste0(output_dir, "03_02_MAST_command_lines.txt")
+write(MAST_run_df$MAST_iqtree2_command, file = MAST_call_text_path)
 # Run HMMster
 if (run.MAST == TRUE){
   # Call IQ-Tree2
-  system(HMMster_df$HMMster_iqtree2_command)
+  system(MAST_run_df$MAST_iqtree2_command)
 }
 
 
 
-#### 6. Extract results from phyloHMM and HMMster runs ####
+#### 5. Extract results from MAST runs ####
 ## Extract file paths
-# Extract list of HMMster and phyloHMM files
+# Extract list of MAST files
 all_files <- list.files(mast_dir, recursive = TRUE)
 exclude_dirs <- "minbl_0.00001"
 op_files <- all_files[grep(exclude_dirs, dirname(all_files), invert = T)]
-phylohmm_files <- grep("\\.hmm", grep("phyloHMM", op_files, value = T), value = T)
-hmmster_files <- grep("\\.hmm", grep("HMMster", op_files, value = T), value = T)
-## HMM results and site ratios
-# To extract information from the completed HMM run:
-phylohmm_hmm_output <- lapply(paste0(mast_dir, phylohmm_files), extract.HMM.output)
-# To extract information from the completed HMM run:
-hmmster_hmm_output <- lapply(paste0(mast_dir, hmmster_files), extract.HMM.output)
+mast_files <- grep("\\.iqtree", grep("MAST", op_files, value = T), value = T)
+
 ## Tree Weights
 # To extract information from the tree weights:
-phylohmm_tw_output <- lapply(paste0(mast_dir, gsub("\\.hmm", ".iqtree", phylohmm_files)), extract.tree.weights)
-# To extract information from the tree weights:
-hmmster_tw_output <- lapply(paste0(mast_dir, gsub("\\.hmm", ".iqtree", hmmster_files)), extract.tree.weights)
-## Create HMM dataframe
-hmm_df <- rbind(as.data.frame(do.call(rbind, phylohmm_hmm_output)), as.data.frame(do.call(rbind, hmmster_hmm_output)))
-## Create tree weight dataframe
-tw_df <- rbind(as.data.frame(do.call(rbind, phylohmm_tw_output)), as.data.frame(do.call(rbind, hmmster_tw_output)))
+mast_tws_df <- as.data.frame(do.call(rbind, lapply(mast_files, extract.tree.weights)))
+
 ## Format dataframes
-hmm_df$dataset <- unlist(lapply(1:nrow(hmm_df), function(i){strsplit(hmm_df$hmm_file[i], "\\.")[[1]][1]}))
-hmm_df$matrix_name <- unlist(lapply(1:nrow(hmm_df), function(i){strsplit(hmm_df$hmm_file[i], "\\.")[[1]][2]}))
-hmm_df$model_code <- unlist(lapply(1:nrow(hmm_df), function(i){strsplit(hmm_df$hmm_file[i], "\\.")[[1]][3]}))
-hmm_df$mast_branch_type <- unlist(lapply(hmmster_files, return.MAST.branch.type))
-hmm_df$minimum_branch_length <- paste0("0.", unlist(lapply(1:nrow(hmm_df), function(i){strsplit(hmm_df$hmm_file[i], "\\.")[[1]][7]})))
-hmm_df <- hmm_df[, c("dataset", "matrix_name", "model_code",  "analysis_type",
-                     "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees",
-                     "tree_1_hmm_probs", "tree_2_hmm_probs", "tree_3_hmm_probs", "tree_4_hmm_probs",
-                     "tree_5_hmm_probs", "tree_1_number_sites", "tree_2_number_sites", "tree_3_number_sites",
-                     "tree_4_number_sites", "tree_5_number_sites", "tree_1_ratio_sites", "tree_2_ratio_sites",
-                     "tree_3_ratio_sites", "tree_4_ratio_sites", "tree_5_ratio_sites")]
-names(hmm_df) <- c("hmm_dataset", "hmm_matrix_name", "hmm_model_code",  "hmm_analysis_type", 
-                   "hmm_mast_branch_type", "hmm_minimum_branch_length", "hmm_number_hypothesis_trees",
-                   "tree_1_hmm_probs", "tree_2_hmm_probs", "tree_3_hmm_probs", "tree_4_hmm_probs",
-                   "tree_5_hmm_probs", "tree_1_number_sites", "tree_2_number_sites", "tree_3_number_sites",
-                   "tree_4_number_sites", "tree_5_number_sites", "tree_1_ratio_sites", "tree_2_ratio_sites",
-                   "tree_3_ratio_sites", "tree_4_ratio_sites", "tree_5_ratio_sites")
-hmm_df <- hmm_df[order(hmm_df$hmm_analysis_type, hmm_df$hmm_dataset, hmm_df$hmm_matrix_name, hmm_df$hmm_matrix_name), ]
-tw_df$dataset <- unlist(lapply(1:nrow(tw_df), function(i){strsplit(tw_df$iq_file[i], "\\.")[[1]][1]}))
-tw_df$matrix_name <- unlist(lapply(1:nrow(tw_df), function(i){strsplit(tw_df$iq_file[i], "\\.")[[1]][2]}))
-tw_df$model_code <- unlist(lapply(1:nrow(tw_df), function(i){strsplit(tw_df$iq_file[i], "\\.")[[1]][3]}))
-tw_df$mast_branch_type <- unlist(lapply(hmmster_files, return.MAST.branch.type))
-tw_df$minimum_branch_length <- paste0("0.", unlist(lapply(1:nrow(tw_df), function(i){strsplit(tw_df$iq_file[i], "\\.")[[1]][7]})))
-tw_df <- tw_df[, c("dataset", "matrix_name", "model_code",  "analysis_type", 
-                   "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees",
-                   "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", "tree_4_tree_weight",
-                   "tree_5_tree_weight", "tree_1_total_tree_length", "tree_2_total_tree_length",
-                   "tree_3_total_tree_length", "tree_4_total_tree_length", "tree_5_total_tree_length",
-                   "tree_1_sum_internal_branch_lengths", "tree_2_sum_internal_branch_lengths",
-                   "tree_3_sum_internal_branch_lengths", "tree_4_sum_internal_branch_lengths",
-                   "tree_5_sum_internal_branch_lengths")]
-names(tw_df) <- c("tw_dataset", "tw_matrix_name", "tw_model_code",  "tw_analysis_type", 
-                  "tw_mast_branch_type", "tw_minimum_branch_length", "tw_number_hypothesis_trees",
-                  "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", "tree_4_tree_weight",
-                  "tree_5_tree_weight", "tree_1_total_tree_length", "tree_2_total_tree_length",
-                  "tree_3_total_tree_length", "tree_4_total_tree_length", "tree_5_total_tree_length",
-                  "tree_1_sum_internal_branch_lengths", "tree_2_sum_internal_branch_lengths",
-                  "tree_3_sum_internal_branch_lengths", "tree_4_sum_internal_branch_lengths",
-                  "tree_5_sum_internal_branch_lengths")
-tw_df <- tw_df[order(tw_df$tw_analysis_type, tw_df$tw_dataset, tw_df$tw_matrix_name, tw_df$tw_matrix_name), ]
-## Combine dataframes
-mast_df <- cbind(hmm_df, tw_df)
-mast_df <- mast_df[, c("hmm_dataset", "hmm_matrix_name", "hmm_model_code",  "hmm_analysis_type", 
-                       "hmm_mast_branch_type", "hmm_minimum_branch_length", "hmm_number_hypothesis_trees",
-                       "tree_1_hmm_probs", "tree_2_hmm_probs", "tree_3_hmm_probs", "tree_4_hmm_probs",
-                       "tree_5_hmm_probs", "tree_1_number_sites", "tree_2_number_sites", "tree_3_number_sites",
-                       "tree_4_number_sites", "tree_5_number_sites", "tree_1_ratio_sites", "tree_2_ratio_sites",
-                       "tree_3_ratio_sites", "tree_4_ratio_sites", "tree_5_ratio_sites",
-                       "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", "tree_4_tree_weight",
-                       "tree_5_tree_weight", "tree_1_total_tree_length", "tree_2_total_tree_length",
-                       "tree_3_total_tree_length", "tree_4_total_tree_length", "tree_5_total_tree_length",
-                       "tree_1_sum_internal_branch_lengths", "tree_2_sum_internal_branch_lengths",
-                       "tree_3_sum_internal_branch_lengths", "tree_4_sum_internal_branch_lengths",
-                       "tree_5_sum_internal_branch_lengths")]
-names(mast_df) <- c("dataset", "matrix_name", "model_code",  "analysis_type", 
-                    "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees",
-                    "tree_1_hmm_probs", "tree_2_hmm_probs", "tree_3_hmm_probs", "tree_4_hmm_probs",
-                    "tree_5_hmm_probs", "tree_1_number_sites", "tree_2_number_sites", "tree_3_number_sites",
-                    "tree_4_number_sites", "tree_5_number_sites", "tree_1_ratio_sites", "tree_2_ratio_sites",
-                    "tree_3_ratio_sites", "tree_4_ratio_sites", "tree_5_ratio_sites",
-                    "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", "tree_4_tree_weight",
-                    "tree_5_tree_weight", "tree_1_total_tree_length", "tree_2_total_tree_length",
-                    "tree_3_total_tree_length", "tree_4_total_tree_length", "tree_5_total_tree_length",
-                    "tree_1_sum_internal_branch_lengths", "tree_2_sum_internal_branch_lengths",
-                    "tree_3_sum_internal_branch_lengths", "tree_4_sum_internal_branch_lengths",
-                    "tree_5_sum_internal_branch_lengths")
+mast_tws_df$dataset <- unlist(lapply(1:nrow(mast_tws_df), function(i){strsplit(mast_tws_df$iq_file[i], "\\.")[[1]][1]}))
+mast_tws_df$matrix_name <- unlist(lapply(1:nrow(mast_tws_df), function(i){strsplit(mast_tws_df$iq_file[i], "\\.")[[1]][2]}))
+mast_tws_df$model_code <- unlist(lapply(1:nrow(mast_tws_df), function(i){strsplit(mast_tws_df$iq_file[i], "\\.")[[1]][3]}))
+mast_tws_df$mast_branch_type <- unlist(lapply(hmmster_files, return.MAST.branch.type))
+mast_tws_df$minimum_branch_length <- paste0("0.", unlist(lapply(1:nrow(mast_tws_df), function(i){strsplit(mast_tws_df$iq_file[i], "\\.")[[1]][7]})))
+mast_tws_df <- mast_tws_df[, c("dataset", "matrix_name", "model_code",  "analysis_type", 
+                               "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees",
+                               "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", "tree_4_tree_weight",
+                               "tree_5_tree_weight", "tree_1_total_tree_length", "tree_2_total_tree_length",
+                               "tree_3_total_tree_length", "tree_4_total_tree_length", "tree_5_total_tree_length",
+                               "tree_1_sum_internal_branch_lengths", "tree_2_sum_internal_branch_lengths",
+                               "tree_3_sum_internal_branch_lengths", "tree_4_sum_internal_branch_lengths",
+                               "tree_5_sum_internal_branch_lengths")]
+names(mast_tws_df) <- c("dataset", "matrix_name", "model_code",  "analysis_type", 
+                        "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees",
+                        "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", "tree_4_tree_weight",
+                        "tree_5_tree_weight", "tree_1_total_tree_length", "tree_2_total_tree_length",
+                        "tree_3_total_tree_length", "tree_4_total_tree_length", "tree_5_total_tree_length",
+                        "tree_1_sum_internal_branch_lengths", "tree_2_sum_internal_branch_lengths",
+                        "tree_3_sum_internal_branch_lengths", "tree_4_sum_internal_branch_lengths",
+                        "tree_5_sum_internal_branch_lengths")
+mast_tws_df <- mast_tws_df[order(mast_tws_df$analysis_type, mast_tws_df$mast_branch_type, 
+                                 mast_tws_df$dataset, mast_tws_df$matrix_name, mast_tws_df$model_code), ]
+
 ## Save output dataframe
 mast_df_file <- paste0(output_dir, "04_01_MAST_model_output.tsv")
-write.table(mast_df, mast_df_file, sep= "\t")
+write.table(mast_tws_df, mast_df_file, sep= "\t")
 
 
 
