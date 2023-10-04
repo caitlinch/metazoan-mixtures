779f446967006073cac8fe36cdb57439c4b051be
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-10-04 12:27:38 +1100

    Update file names for plotting. Update color palette selection and remove unused palettes.

--- code/05_plots.R
+++ code/05_plots.R
@@ -18,7 +18,8 @@ tree_dir <- "/Users/caitlincherryh/Documents/C3_TreeMixtures_Sponges/04_output/0
 repo_dir <- "/Users/caitlincherryh/Documents/Repositories/metazoan-mixtures/"
 
 ## Control parameters
-control_parameters <- list(plot.hypothesis.trees = FALSE,
+control_parameters <- list(add.extra.color.palettes = FALSE,
+                           plot.hypothesis.trees = FALSE,
                            plot.MAST = TRUE,
                            plot.AU.tests = TRUE,
                            plot.ELW = TRUE,
@@ -41,15 +42,20 @@ library(dplyr)
 source(paste0(repo_dir,"code/func_plotting.R"))
 source(paste0(repo_dir,"code/func_data_processing.R"))
 
-# Specify colour palettes (some of these are unused)
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# Specify colour palettes used within these plots
 metazoan_palette <- c(A = "#CC79A7", B = "#009E73", C = "#56B4E9", D = "#E69F00", E = "#999999")
-metazoan_clade_palette <- c(Bilateria = "#CC79A7", Cnidaria = "#009E73", Ctenophora = "#56B4E9", Porifera = "#E69F00", Outgroup = "#999999")
-tree2_palette <- c("#F0F921FF", "#0D0887FF")
-tree5_palette <- c("#e66101", "#fdb863", "#f7f7f7", "#b2abd2", "#5e3c99")
-tree5_cividis <- c("#FDE725FF", "#5DC863FF", "#21908CFF", "#3B528BFF", "#440154FF")
-tree2_cividis <- c(tree5_cividis[1], tree5_cividis[5])
-tree2_tonal <- c("#bdd7e7", "#2171b5")
+model3_tonal <- c("#980043", "#df65b0", "#d4b9da")
+
+# Extra colour palettes (unused)
+if (add.extra.color.palettes = TRUE){
+  cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  metazoan_clade_palette <- c(Bilateria = "#CC79A7", Cnidaria = "#009E73", Ctenophora = "#56B4E9", Porifera = "#E69F00", Outgroup = "#999999")
+  tree2_palette <- c("#F0F921FF", "#0D0887FF")
+  tree5_palette <- c("#e66101", "#fdb863", "#f7f7f7", "#b2abd2", "#5e3c99")
+  tree5_cividis <- c("#FDE725FF", "#5DC863FF", "#21908CFF", "#3B528BFF", "#440154FF")
+  tree2_cividis <- c(tree5_cividis[1], tree5_cividis[5])
+  tree2_tonal <- c("#bdd7e7", "#2171b5")
+}
 
 # List all files
 all_files <- list.files(results_dir, recursive = TRUE)
@@ -113,7 +119,7 @@ if (control_parameters$plot.hypothesis.trees == TRUE){
 #### 3. Plot tree weights from MAST model ####
 if (control_parameters$plot.MAST == TRUE){
   # Identify MAST csv file
-  mast_df_file <- paste0(results_dir, grep("summary_MAST_treeWeight_results.csv", all_files, value = TRUE))
+  mast_df_file <- paste0(results_dir, grep("summary_MAST_treeWeight_results", all_files, value = TRUE))
   mast_df <- read.csv(mast_df_file, header = TRUE)
   # Convert MAST output to long format
   mast_long <- melt(mast_df,
@@ -141,7 +147,7 @@ if (control_parameters$plot.MAST == TRUE){
     scale_x_discrete(name = NULL) +
     scale_y_continuous(name = "Tree weight", limits = c(0,1), breaks = seq(0,1,0.2), labels = seq(0,1,0.2), minor_breaks = seq(0,1,0.1)) +
     labs(title = "MAST tree weights") +
-    scale_color_manual(name = "Model class", labels = c("CXX", "PMSF", "Other"), values = c("#d4b9da", "#df65b0", "#980043")) +
+    scale_color_manual(name = "Model class", labels = c("CXX", "PMSF", "Other"), values = model3_tonal) +
     theme_bw() +
     theme(axis.title.y = element_text(size = 25, margin = margin(t = 0, r = 15, b = 0, l = 10)),
           axis.text.x = element_text(size = 15, vjust = 0.5, hjust = 1, angle = 90, margin = margin(t = 10, r = 0, b = 10, l = 0)),  
@@ -150,7 +156,7 @@ if (control_parameters$plot.MAST == TRUE){
           plot.title = element_text(size = 40, hjust = 0.5, margin = margin(t = 10, r = 0, b = 15, l = 0)),
           legend.title = element_text(size = 20),
           legend.text = element_text(size = 15) )
-  bp_file <- paste0(plot_dir, "mainfig_MAST_tree_weights_2tree.")
+  bp_file <- paste0(plot_dir, "mainfigure_MAST_tree_weights_2tree.")
   ggsave(filename = paste0(bp_file, "png"), plot = bp, device = "png", width = 12, height = 14, units = "in")
   ggsave(filename = paste0(bp_file, "pdf"), plot = bp, device = "pdf", width = 12, height = 14, units = "in")
 }
@@ -160,7 +166,7 @@ if (control_parameters$plot.MAST == TRUE){
 #### 4. Plot AU test results from tree topology tests ####
 if (control_parameters$plot.AU.test == TRUE){
   # Identify AU test results
-  au_df_file <- paste0(results_dir, grep("summary_au_test_results.csv", all_files, value = TRUE))
+  au_df_file <- paste0(results_dir, grep("summary_au_test_results", all_files, value = TRUE))
   au_df <- read.csv(au_df_file, header = TRUE)
   # Convert AU test output to long format
   au_long <- melt(au_df,
@@ -188,7 +194,7 @@ if (control_parameters$plot.AU.test == TRUE){
     facet_wrap(~dataset_label) +
     scale_x_discrete(name = NULL) +
     scale_y_continuous(name = "p-value", limits = c(0,1), breaks = seq(0,1,0.2), labels = seq(0,1,0.2), minor_breaks = seq(0,1,0.1)) +
-    scale_color_manual(name = "Model class", labels = c("CXX", "PMSF", "Other"), values = c("#d4b9da", "#df65b0", "#980043")) +
+    scale_color_manual(name = "Model class", labels = c("CXX", "PMSF", "Other"), values = model3_tonal) +
     labs(title = "AU Test") +
     theme_bw() +
     theme(axis.title.y = element_text(size = 25, margin = margin(t = 0, r = 15, b = 0, l = 10)),
@@ -198,7 +204,7 @@ if (control_parameters$plot.AU.test == TRUE){
           plot.title = element_text(size = 40, hjust = 0.5, margin = margin(t = 10, r = 0, b = 15, l = 0)),
           legend.title = element_text(size = 20),
           legend.text = element_text(size = 15) )
-  bp_file <- paste0(plot_dir, "mainfig_au_test_2tree.")
+  bp_file <- paste0(plot_dir, "mainfigure_au_test_2tree.")
   ggsave(filename = paste0(bp_file, "png"), plot = bp, device = "png", width = 12, height = 14, units = "in")
   ggsave(filename = paste0(bp_file, "pdf"), plot = bp, device = "pdf", width = 12, height = 14, units = "in")
 }
@@ -208,7 +214,7 @@ if (control_parameters$plot.AU.test == TRUE){
 #### 5. Plot expected likelihood weights from tree topology tests ####
 if (control_parameters$plot.ELW == TRUE){
   # Identify ELW results
-  elw_df_file <- paste0(results_dir, grep("summary_elw_results.csv", all_files, value = TRUE))
+  elw_df_file <- paste0(results_dir, grep("summary_elw_results", all_files, value = TRUE))
   elw_df <- read.csv(elw_df_file, header = TRUE)
   # Convert ELW output to long format
   elw_long <- melt(elw_df,
@@ -230,12 +236,12 @@ if (control_parameters$plot.ELW == TRUE){
                                    ordered = TRUE)
   # Plot with boxplot for each dataset
   bp <- ggplot(elw_long, aes(x = var_label, y = value, color = model_class, group = model_class)) +
-    geom_point(size = 3, alpha = 0.6) +
+    geom_point(size = 3, alpha = 0.6) + 
     geom_line(alpha = 0.6) +
     facet_wrap(~dataset_label) +
     scale_x_discrete(name = NULL) +
     scale_y_continuous(name = "Weight", limits = c(0,1), breaks = seq(0,1,0.2), labels = seq(0,1,0.2), minor_breaks = seq(0,1,0.1)) +
-    scale_color_manual(name = "Model class", labels = c("CXX", "PMSF", "Other"), values = c("#d4b9da", "#df65b0", "#980043")) +
+    scale_color_manual(name = "Model class", labels = c("CXX", "PMSF", "Other"), values = model3_tonal) +
     labs(title = "Expected likelihood weight") +
     theme_bw() +
     theme(axis.title.y = element_text(size = 25, margin = margin(t = 0, r = 15, b = 0, l = 10)),
@@ -245,7 +251,7 @@ if (control_parameters$plot.ELW == TRUE){
           plot.title = element_text(size = 40, hjust = 0.5, margin = margin(t = 10, r = 0, b = 15, l = 0)),
           legend.title = element_text(size = 20),
           legend.text = element_text(size = 15) )
-  bp_file <- paste0(plot_dir, "mainfig_expected_likelihood_weights_2tree.")
+  bp_file <- paste0(plot_dir, "mainfigure_expected_likelihood_weights_2tree.")
   ggsave(filename = paste0(bp_file, "png"), plot = bp, device = "png", width = 12, height = 14, units = "in")
   ggsave(filename = paste0(bp_file, "pdf"), plot = bp, device = "pdf", width = 12, height = 14, units = "in")
 }
@@ -255,8 +261,7 @@ if (control_parameters$plot.ELW == TRUE){
 #### 6. Summarise maximum likelihood topology tree results ####
 if (control_parameters$plot.ML.topologies == TRUE | control_parameters$plot.Porifera.topologies == TRUE){
   ## Plot tree topology
-  # List all output files
-  all_files <- list.files(results_dir, recursive = TRUE)
+  # Open dataframe
   topo_df_file <- grep("xls", grep("ML_tree_topology_ManualCheck", all_files, value = TRUE), value = TRUE)
   topo_df_file <- grep("Summary", topo_df_file, value = TRUE, invert = TRUE)
   topo_df <- as.data.frame(read_excel(path = paste0(results_dir, "/", topo_df_file), sheet = "Topology"))
@@ -371,6 +376,36 @@ if (control_parameters$plot.ML.topologies == TRUE | control_parameters$plot.Pori
 #### 7. Look at model class for maximum likelihood topology tree results ####
 if (control_parameters$plot.ML.topologies == TRUE | control_parameters$plot.Porifera.topologies == TRUE){
   ## Plot tree topology
+  # Open dataframe
+  topo_df_file <- grep("xls", grep("ML_tree_topology_ManualCheck", all_files, value = TRUE), value = TRUE)
+  topo_df_file <- grep("Summary", topo_df_file, value = TRUE, invert = TRUE)
+  topo_df <- as.data.frame(read_excel(path = paste0(results_dir, "/", topo_df_file), sheet = "Topology"))
+  # Convert topology output to long format
+  topo_long <- melt(topo_df,
+                    id.vars = c("dataset", "matrix_name", "model_code", "PORI_topology"),
+                    measure.vars = c("sister_group"))
+  topo_long$dataset_label <- factor(topo_long$matrix_name,
+                                    levels = c("Dunn2008_FixedNames", "Philippe_etal_superalignment_FixedNames", "Pick2010",
+                                               "UPDUNN_MB_FixedNames", "nonribosomal_9187_smatrix", "ribosomal_14615_smatrix",
+                                               "REA_EST_includingXenoturbella", "ED3d", "Best108", "Chang_AA",  "Dataset10",
+                                               "Metazoa_Choano_RCFV_strict", "Tplx_phylo_d1", "nonbilateria_MARE_BMGE"),
+                                    labels = c("Dunn 2008", "Philippe 2009", "Pick 2010", "Philippe 2011", "Nosenko 2013\nnon-ribosomal", 
+                                               "Nosenko 2013\nribosomal", "Ryan 2013", "Moroz 2014", "Borowiec 2015", "Chang 2015", 
+                                               "Whelan 2015", "Whelan 2017", "Laumer 2018", "Laumer 2019" ),
+                                    ordered = TRUE)
+  topo_long$dataset_label_singleLine <- factor(topo_long$matrix_name,
+                                               levels = c("Dunn2008_FixedNames", "Philippe_etal_superalignment_FixedNames", "Pick2010",
+                                                          "UPDUNN_MB_FixedNames", "nonribosomal_9187_smatrix", "ribosomal_14615_smatrix",
+                                                          "REA_EST_includingXenoturbella", "ED3d", "Best108", "Chang_AA",  "Dataset10",
+                                                          "Metazoa_Choano_RCFV_strict", "Tplx_phylo_d1", "nonbilateria_MARE_BMGE"),
+                                               labels = c("Dunn 2008", "Philippe 2009", "Pick 2010", "Philippe 2011", "Nosenko 2013 non-ribosomal", 
+                                                          "Nosenko 2013 ribosomal", "Ryan 2013", "Moroz 2014", "Borowiec 2015", "Chang 2015", 
+                                                          "Whelan 2015", "Whelan 2017", "Laumer 2018", "Laumer 2019" ),
+                                               ordered = TRUE)
+  topo_long$PORI_topology <- factor(topo_long$PORI_topology,
+                                    levels = c("One taxon", "Monophyletic", "Paraphyletic"),
+                                    labels = c("One taxon", "Monophyletic", "Paraphyletic"),
+                                    ordered = TRUE)
   # Add model class to the topology_df
   #   Matrix is for single matrix amino-acid exchange rate matrices
   #   Mixture model is for protein mixture models from IQ-Tree
