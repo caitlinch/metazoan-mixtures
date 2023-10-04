591899a29da18d2af4e82f07e5122a94e51112c0
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-10-04 12:09:03 +1100

    Output separate files for 2 and 5 tree runs for MAST and AU test analysis. Output any ML trees with differeing BIC values from manual and automatic extraction (allows you to ensure you have all correct BIC scores and thus all correct models.

--- code/04_reformat_output_dataframes.R
+++ code/04_reformat_output_dataframes.R
@@ -107,44 +107,59 @@ if (file.exists(pori_topology_file) == FALSE){
 #### 4. Prepare summary of the AU test results using the tree topology test tsv file ####
 # Read in csv file
 tree_test_df <- read.csv(file = grep("tree_topology_test_results.csv", all_output_files, value = TRUE), header = TRUE)
-# Process each dataset one at a time
-summary_au_test_df <- as.data.frame(do.call(rbind, lapply(unique(tree_test_df$ID), summarise.AU.test.results, tree_test_df)))
-# Add new column for model class
-summary_au_test_df$model_class <- factor(summary_au_test_df$best_model_code,
-                                         levels = c("LG_C60", "C60", "PMSF_C60", "PMSF_LG_C60", "LG4M", "UL3"),
-                                         labels = c("CXX", "CXX", "PMSF", "PMSF", "Other", "Other"),
-                                         ordered = TRUE)
-# Sort output by year
-summary_au_test_df <- summary_au_test_df[order(summary_au_test_df$year, summary_au_test_df$dataset, summary_au_test_df$matrix),]
-# Reorder columns
-summary_au_test_df <- summary_au_test_df[, c("dataset", "matrix", "model_class", "best_model_code", "topology_test", "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")]
-# Remove columns consisting only of NA
-summary_au_test_df <- Filter(function(x)!all(is.na(x)), summary_au_test_df)
-# Write the output
-summary_au_test_file <- paste0(output_file_dir, "summary_au_test_results.csv")
-write.csv(summary_au_test_df, file = summary_au_test_file, row.names = FALSE)
+# Process for 2trees and 5trees analysis
+analyses <- c("2_trees", "5_trees")
+for (a in analyses){
+  # Process each dataset one at a time
+  temp_tree_test <- tree_test_df[tree_test_df$hypothesis_tree_analysis == a, ]
+  summary_au_test_df <- as.data.frame(do.call(rbind, lapply(unique(temp_tree_test$ID), summarise.AU.test.results, temp_tree_test)))
+  # Add new columns for model class and hypothesis tree analysis
+  summary_au_test_df$model_class <- factor(summary_au_test_df$best_model_code,
+                                           levels = c("LG_C60", "C60", "LG_C20", "PMSF_C60", "PMSF_LG_C60", "LG4M", "UL3"),
+                                           labels = c("CXX", "CXX", "CXX", "PMSF", "PMSF", "Other", "Other"),
+                                           ordered = TRUE)
+  summary_au_test_df$hypothesis_tree_analysis <- a
+  # Sort output by year
+  summary_au_test_df <- summary_au_test_df[order(summary_au_test_df$year, summary_au_test_df$dataset, summary_au_test_df$matrix),]
+  # Reorder columns
+  summary_au_test_df <- summary_au_test_df[, c("hypothesis_tree_analysis", "dataset", "matrix", "model_class", "best_model_code",
+                                               "topology_test", "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")]
+  # Remove columns consisting only of NA
+  summary_au_test_df <- Filter(function(x)!all(is.na(x)), summary_au_test_df)
+  # Write the output
+  summary_au_test_file <- paste0(output_file_dir, "summary_au_test_results_", gsub("_", "", a), ".csv")
+  write.csv(summary_au_test_df, file = summary_au_test_file, row.names = FALSE)
+}
 
 
 
 #### 5. Prepare summary of the expected likelihood weights using the tree topology test tsv file ####
 # Read in csv file
 tree_test_df <- read.csv(file = grep("tree_topology_test_results.csv", all_output_files, value = TRUE), header = TRUE)
-# Process each dataset one at a time
-summary_elw_test_df <- as.data.frame(do.call(rbind, lapply(unique(tree_test_df$ID), summarise.eLW, tree_test_df)))
-# Add new column for model class
-summary_elw_test_df$model_class <- factor(summary_elw_test_df$best_model_code,
-                                          levels = c("LG_C60", "C60", "PMSF_C60", "PMSF_LG_C60", "LG4M", "UL3"),
-                                          labels = c("CXX", "CXX", "PMSF", "PMSF", "Other", "Other"),
-                                          ordered = TRUE)
-# Sort output by year
-summary_elw_test_df <- summary_elw_test_df[order(summary_elw_test_df$year, summary_elw_test_df$dataset, summary_elw_test_df$matrix),]
-# Reorder columns
-summary_elw_test_df <- summary_elw_test_df[, c("dataset", "matrix", "model_class", "best_model_code", "topology_test", "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")]
-# Remove columns consisting only of NA
-summary_elw_test_df <- Filter(function(x)!all(is.na(x)), summary_elw_test_df)
-# Write the output
-summary_elw_test_file <- paste0(output_file_dir, "summary_elw_results.csv")
-write.csv(summary_elw_test_df, file = summary_elw_test_file, row.names = FALSE)
+# Process for 2trees and 5trees analysis
+analyses <- c("2_trees", "5_trees")
+for (a in analyses){
+  # Process each dataset one at a time
+  temp_tree_test <- tree_test_df[tree_test_df$hypothesis_tree_analysis == a, ]
+  # Process each dataset one at a time
+  summary_elw_test_df <- as.data.frame(do.call(rbind, lapply(unique(temp_tree_test$ID), summarise.eLW, temp_tree_test)))
+  # Add new column for model class
+  summary_elw_test_df$model_class <- factor(summary_elw_test_df$best_model_code,
+                                            levels = c("LG_C60", "C60", "LG_C20", "PMSF_C60", "PMSF_LG_C60", "LG4M", "UL3"),
+                                            labels = c("CXX", "CXX", "CXX", "PMSF", "PMSF", "Other", "Other"),
+                                            ordered = TRUE)
+  summary_elw_test_df$hypothesis_tree_analysis <- a
+  # Sort output by year
+  summary_elw_test_df <- summary_elw_test_df[order(summary_elw_test_df$year, summary_elw_test_df$dataset, summary_elw_test_df$matrix),]
+  # Reorder columns
+  summary_elw_test_df <- summary_elw_test_df[, c("hypothesis_tree_analysis", "dataset", "matrix", "model_class", "best_model_code",
+                                                 "topology_test", "tree_1", "tree_2", "tree_3", "tree_4", "tree_5", "year")]
+  # Remove columns consisting only of NA
+  summary_elw_test_df <- Filter(function(x)!all(is.na(x)), summary_elw_test_df)
+  # Write the output
+  summary_elw_test_file <- paste0(output_file_dir, "summary_elw_results_", gsub("_", "", a), ".csv")
+  write.csv(summary_elw_test_df, file = summary_elw_test_file, row.names = FALSE)
+}
 
 
 
@@ -152,68 +167,87 @@ write.csv(summary_elw_test_df, file = summary_elw_test_file, row.names = FALSE)
 ### Output site ratios from HMM weights
 # Read in tsv file
 mast_df <- read.csv(file = grep("MAST_model_output", all_output_files, value = TRUE), header = TRUE)
-# Output tree weights
-# Process each dataset one at a time
-mast_summary_df <- as.data.frame(do.call(rbind, lapply(1:nrow(mast_df), summarise.tree.weights, tw_df = mast_df)))
-# Sort output by year
-mast_summary_df <- mast_summary_df[order(mast_summary_df$year, mast_summary_df$dataset, mast_summary_df$matrix_name), ]
-# Reorganise columns
-mast_summary_df <- mast_summary_df[,c("dataset", "matrix_name", "model_class", "model_code", 
-                                      "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", 
-                                      "tree_4_tree_weight", "tree_5_tree_weight",
-                                      "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees", "year")]
-# Remove columns consisting only of NA
-mast_summary_df <- Filter(function(x)!all(is.na(x)), mast_summary_df)
-# Write the output for MAST tree weights
-write.csv(mast_summary_df, file = paste0(output_file_dir, "summary_MAST_treeWeight_results.csv"), row.names = FALSE)
+# Process for 2trees and 5trees analysis
+analyses <- c("2_trees", "5_trees")
+for (a in analyses){
+  # Output tree weights
+  # Process each dataset one at a time
+  temp_tree_test <- mast_df[mast_df$hypothesis_tree_analysis == a, ]
+  mast_summary_df <- as.data.frame(do.call(rbind, lapply(1:nrow(temp_tree_test), summarise.tree.weights, tw_df = temp_tree_test)))
+  # Sort output by year
+  mast_summary_df <- mast_summary_df[order(mast_summary_df$year, mast_summary_df$dataset, mast_summary_df$matrix_name), ]
+  mast_summary_df$hypothesis_tree_analysis <- a
+  # Reorganise columns
+  mast_summary_df <- mast_summary_df[,c("hypothesis_tree_analysis", "dataset", "matrix_name", "model_class", "model_code", 
+                                        "tree_1_tree_weight", "tree_2_tree_weight", "tree_3_tree_weight", 
+                                        "tree_4_tree_weight", "tree_5_tree_weight",
+                                        "mast_branch_type", "minimum_branch_length", "number_hypothesis_trees", "year")]
+  # Remove columns consisting only of NA
+  mast_summary_df <- Filter(function(x)!all(is.na(x)), mast_summary_df)
+  # Write the output for MAST tree weights
+  write.csv(mast_summary_df, file = paste0(output_file_dir, "summary_MAST_treeWeight_results_", gsub("_", "", a), ".csv"), row.names = FALSE)
+}
 
 
 
 #### 6. Check and compare manually extracted BIC/tree BIC results with the automatically extracted values ####
-# Set order for models and datasets
-model_order <- c("PMSF_C20", "PMSF_C60", "PMSF_LG_C20", "PMSF_LG_C60", 
-                 "C20", "C60", "LG_C20", "LG_C60", "CF4", "EHO", "EX_EHO",
-                 "EX2", "EX3", "GTR20", "JTT", "JTTDCMut", "LG", "LG4M",
-                 "mtZOA", "PMB", "Poisson", "rtREV", "UL2", "UL3", "WAG",
-                 "ModelFinder")
-dataset_order <- c("Dunn2008", "Philippe2009", "Pick2010", "Philippe2011",
-                   "Nosenko2013", "Ryan2013", "Moroz2014", "Borowiec2015",
-                   "Chang2015", "Whelan2015", "Whelan2017", "Laumer2018",
-                   "Laumer2019")
-# Open the ML tree results and trim the dataframes
-ml_results_file <- grep("maximum_likelihood_results", all_output_files, value = T)
-ml_results_df <- read.table(ml_results_file, header = TRUE, sep = "\t")
-# Fix matrix names to be identical to those in the manual check .xlsx file
-ml_results_df$matrix_name[which(ml_results_df$matrix_name == "Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned")] <- "Dataset10"
-# Trim the ML tree results dataframe
-ml_keep_rows <- sort(intersect(which(ml_results_df$dataset %in% dataset_order), which(ml_results_df$model_code %in% model_order)))
-ml_trimmed_df <- ml_results_df[ml_keep_rows, ]
-# Trim the topology_check_df dataframe
-t_keep_rows <- sort(intersect(which(topology_check_df$dataset %in% dataset_order), which(topology_check_df$model_code %in% model_order)))
-topology_trimmed_df <- topology_check_df[t_keep_rows, ]
-# Update prefix columns
-ml_trimmed_df$prefix <- paste0(ml_trimmed_df$dataset, ".", ml_trimmed_df$matrix_name, ".", ml_trimmed_df$model_code)
-# Trim topology_df to only inclue columns in the ml_trimmed_df
-topology_trimmed_df <- topology_trimmed_df[which(topology_trimmed_df$ML_tree_name %in% ml_trimmed_df$prefix), ]
-# Sort dataframe row order
-topology_trimmed_df <- topology_trimmed_df[order(topology_trimmed_df$dataset, topology_trimmed_df$matrix_name, topology_trimmed_df$model_code), ]
-ml_trimmed_df <- ml_trimmed_df[order(ml_trimmed_df$dataset, ml_trimmed_df$matrix_name, ml_trimmed_df$model_code), ]
-# Combine relevant columns into a single dataframe
-comparison_df <- as.data.frame(cbind(ml_trimmed_df$dataset, ml_trimmed_df$model_code, 
-                                     ml_trimmed_df$matrix_name, ml_trimmed_df$prefix,
-                                     ml_trimmed_df$best_model_BIC, topology_trimmed_df$model_BIC,
-                                     ml_trimmed_df$tree_BIC, topology_trimmed_df$tree_BIC))
-names(comparison_df) <- c("dataset", "model_code",
-                          "matrix_name", "prefix",
-                          "extracted_model_BIC", "manual_model_BIC",
-                          "extracted_tree_BIC", "manual_tree_BIC")
-# Identify which model_BIC and tree_BIC differ in the two files and print results
-differing_model_BIC_rows <- which(comparison_df$extracted_model_BIC != comparison_df$manual_model_BIC)
-differing_tree_BIC_rows <- which(comparison_df$extracted_tree_BIC != comparison_df$manual_tree_BIC)
-print(paste0("Number of best model BIC values differing between automatic and manual extraction: ", length(differing_model_BIC_rows)))
-if (length(differing_model_BIC_rows) > 0){print(paste0("Rows differing for model BIC: ", differing_model_BIC_rows))}
-print(paste0("Number of tree BIC values differing between automatic and manual extraction: ", length(differing_tree_BIC_rows)))
-if (length(differing_tree_BIC_rows) > 0){print(paste0("Rows differing for tree BIC: ", differing_tree_BIC_rows))}
-# Manually check and correct any rows with differing BIC values (for best model or for trees)
+compare_BIC_file = paste0(output_file_dir, "qualityCheck_ML_BIC_comparisons_error.csv")
+# Create the compare_BIC_df if it doesn't exist
+if (file.exists(compare_BIC_df) == FALSE){
+  # Set order for models and datasets
+  model_order <- c("PMSF_C20", "PMSF_C60", "PMSF_LG_C20", "PMSF_LG_C60", 
+                   "C20", "C60", "LG_C20", "LG_C60", "CF4", "EHO", "EX_EHO",
+                   "EX2", "EX3", "GTR20", "JTT", "JTTDCMut", "LG", "LG4M",
+                   "mtZOA", "PMB", "Poisson", "rtREV", "UL2", "UL3", "WAG",
+                   "ModelFinder")
+  dataset_order <- c("Dunn2008", "Philippe2009", "Pick2010", "Philippe2011",
+                     "Nosenko2013", "Ryan2013", "Moroz2014", "Borowiec2015",
+                     "Chang2015", "Whelan2015", "Whelan2017", "Laumer2018",
+                     "Laumer2019")
+  # Open the ML tree results and trim the dataframes
+  ml_results_file <- grep("maximum_likelihood_results", all_output_files, value = T)
+  ml_results_df <- read.table(ml_results_file, header = TRUE, sep = "\t")
+  # Fix matrix names to be identical to those in the manual check .xlsx file
+  ml_results_df$matrix_name[which(ml_results_df$matrix_name == "Dataset10_CertainPruned_LBAtaxa_LBAandHeteroGenesPruned")] <- "Dataset10"
+  # Trim the ML tree results dataframe
+  ml_keep_rows <- sort(intersect(which(ml_results_df$dataset %in% dataset_order), which(ml_results_df$model_code %in% model_order)))
+  ml_trimmed_df <- ml_results_df[ml_keep_rows, ]
+  # Read in .xlsx file with manual topology check results
+  topology_check_x_file <- grep("xls", grep("ML_tree_topology_ManualCheck", all_output_files, value = TRUE), value = TRUE)
+  topology_check_x_file <- grep("Summary", topology_check_x_file, value = TRUE, invert = TRUE)
+  topology_check_df <- as.data.frame(read_excel(path = topology_check_x_file, sheet = "Topology"))
+  # Remove Simion and Hejnol datasets - too computationally intensive to run full ML models
+  topology_check_df <- topology_check_df[which(topology_check_df$dataset != "Hejnol2009" & topology_check_df$dataset != "Simion2017"), ]
+  # Trim the topology_check_df dataframe
+  t_keep_rows <- sort(intersect(which(topology_check_df$dataset %in% dataset_order), which(topology_check_df$model_code %in% model_order)))
+  topology_trimmed_df <- topology_check_df[t_keep_rows, ]
+  # Update prefix columns
+  ml_trimmed_df$prefix <- paste0(ml_trimmed_df$dataset, ".", ml_trimmed_df$matrix_name, ".", ml_trimmed_df$model_code)
+  # Trim topology_df to only inclue columns in the ml_trimmed_df
+  topology_trimmed_df <- topology_trimmed_df[which(topology_trimmed_df$ML_tree_name %in% ml_trimmed_df$prefix), ]
+  # Sort dataframe row order
+  topology_trimmed_df <- topology_trimmed_df[order(topology_trimmed_df$dataset, topology_trimmed_df$matrix_name, topology_trimmed_df$model_code), ]
+  ml_trimmed_df <- ml_trimmed_df[order(ml_trimmed_df$dataset, ml_trimmed_df$matrix_name, ml_trimmed_df$model_code), ]
+  # Combine relevant columns into a single dataframe
+  comparison_df <- as.data.frame(cbind(ml_trimmed_df$dataset, ml_trimmed_df$model_code, 
+                                       ml_trimmed_df$matrix_name, ml_trimmed_df$prefix,
+                                       ml_trimmed_df$best_model_BIC, topology_trimmed_df$model_BIC,
+                                       ml_trimmed_df$tree_BIC, topology_trimmed_df$tree_BIC))
+  names(comparison_df) <- c("dataset", "model_code",
+                            "matrix_name", "prefix",
+                            "extracted_model_BIC", "manual_model_BIC",
+                            "extracted_tree_BIC", "manual_tree_BIC")
+  # Identify which model_BIC and tree_BIC differ in the two files and print results
+  differing_model_BIC_rows <- which(signif(as.numeric(comparison_df$extracted_model_BIC, digits = 10)) != signif(as.numeric(comparison_df$manual_model_BIC, digits = 10)) )
+  differing_tree_BIC_rows <- which(signif(as.numeric(comparison_df$extracted_tree_BIC, digits = 11)) != signif(as.numeric(comparison_df$manual_tree_BIC, digits = 11)) )
+  print(paste0("Number of best model BIC values differing between automatic and manual extraction: ", length(differing_model_BIC_rows)))
+  if (length(differing_model_BIC_rows) > 0){print(paste0("Rows differing for model BIC: ", differing_model_BIC_rows))}
+  print(paste0("Number of tree BIC values differing between automatic and manual extraction: ", length(differing_tree_BIC_rows)))
+  if (length(differing_tree_BIC_rows) > 0){print(paste0("Rows differing for tree BIC: ", differing_tree_BIC_rows))}
+  # Output csv if any rows differ
+  different_df <- comparison_df[c(differing_model_BIC_rows, differing_tree_BIC_rows), ]
+  write.csv(different_df, file = compare_BIC_file, row.names = FALSE)
+  # Note: must manually check and correct any rows with differing BIC values (for best model or for trees)
+}
 
 
